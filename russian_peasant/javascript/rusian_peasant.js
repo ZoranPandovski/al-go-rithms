6f8c17f8ecf750fa0a09da4b45972931579fab71
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-06-10 20:31:24 +0200

    Add russian peasant in js

---
+++ russian_peasant/javascript/rusian_peasant.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var russian_peasant = function(a, b) {
+    let x = a,
+        y = b,
+        sum = 0;
+    while (x > 0) {
+        if (x % 2 == 1) {
+            sum = sum + y;
+        }
+        x = x >> 1
+        y = y << 1
+    }
+    return sum;
+}
