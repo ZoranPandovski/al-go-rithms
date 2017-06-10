f18ef0a589374af457a28bab687beecdbaaae550
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-06-10 20:32:02 +0200

    Add binary search in python

---
+++ search/binary_search/binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(arr, num):
+    found = False
+    first = 0
+    last = len(arr) - 1
+    while first <= last and not found:
+        middle = (first + last)//2
+        if arr[middle] == num:
+            found = True
+        elif arr[middle] > num:
+            first = middle + 1
+        else:
+            first = middle - 1
+    return found
+
+
+def test():
+    list = [2, 5, 7, 8, 78, 99, 102, 5555]
+    item = 5
+    assert if binary_search(list, item) == True
+
+
+if __name__ == '__main__':
+    test()
+
