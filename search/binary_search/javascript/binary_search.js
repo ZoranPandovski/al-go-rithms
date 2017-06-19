9cd2504a58489ab0826a2a623f723b69d58153cb
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-06-20 01:05:02 +0200

    Add assert

--- search/binary_search/javascript/binary_search.js
+++ search/binary_search/javascript/binary_search.js
@@ -1,4 +1,15 @@
+(function(){
 'use strict'
+
+const assert = function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
 
 const binary_search = function(arr, num, start = 0, end = (arr.length - 1)) {
     let middle = Math.floor((start + end) / 2);
@@ -14,3 +25,12 @@ const binary_search = function(arr, num, start = 0, end = (arr.length - 1)) {
     }
     return middle;
 }
+
+
+//test
+const arr = [1,3,5,6,8,11,14,122];
+const num = 12;
+const result = binary_search(arr, num);
+assert(result === num);
+})();
+
