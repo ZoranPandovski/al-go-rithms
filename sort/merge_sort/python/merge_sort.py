0eeb8692ca33713543e617a3d97a9c5c700d7019
Stefan Abi-Karam <stefanabikaram@gmail.com>
2017-09-29 16:01:00 -0400

    added recursive algorithm for merge sort

---
+++ sort/merge_sort/python/merge_sort.py
@@ -0,0 +1,30 @@
+def merge(left, right):
+    """Merge sort merging function."""
+
+    left_index, right_index = 0, 0
+    result = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    """Merge sort algorithm implementation."""
+
+    if len(array) <= 1:  # base case
+        return array
+
+    # divide array in half and merge sort recursively
+    half = len(array) // 2
+    left = merge_sort(array[:half])
+    right = merge_sort(array[half:])
+
+    return merge(left, right)
