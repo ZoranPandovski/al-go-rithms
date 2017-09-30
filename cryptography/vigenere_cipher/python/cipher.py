fea594e6b79e4611a9de4f5a7a04f1de87083a18
csmali <myurdagul@etu.edu.tr>
2017-09-30 21:58:30 +0300

    fixedvigenere

--- cryptography/vigenere_cipher/python/cipher.py
+++ cryptography/vigenere_cipher/python/cipher.py
@@ -1,9 +1,3 @@
-def char_position(letter):
-    return ord(letter) - 97
-
-def pos_to_char(pos):
-    return chr(pos + 97)
-
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 def encrypt(plaintext):
@@ -11,7 +5,7 @@ def encrypt(plaintext):
 	string=""
 	i=0
 	while i<len(plaintext):
-		string=string+str(alphabet[char_position(plaintext[i])+char_position(key[i%len(key)])])
+		string=string+alphabet[alphabet.index(plaintext[i])+alphabet.index(key[i%len(key)])]
 		i=i+1
 	return string
 def decrypt(ciphertext):
@@ -21,7 +15,7 @@ def decrypt(ciphertext):
 	while i<len(ciphertext):
 
 
-		string=string+str(alphabet[alphabet.index(ciphertext[i])-char_position(key[i%len(key)])])
+		string=string+alphabet[alphabet.index(ciphertext[i])-alphabet.index(key[i%len(key)])]
 		i=i+1
 	return string
 
