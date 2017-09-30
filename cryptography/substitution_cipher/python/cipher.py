9f7ef745a989aa7fa7d287c8770ac221d6c72186
csmali <myurdagul@etu.edu.tr>
2017-09-30 20:50:29 +0300

    substitution cipher added

--- cryptography/substitution_cipher/python/cipher.py
+++ cryptography/substitution_cipher/python/cipher.py
@@ -1,15 +1,21 @@
-plaintext = raw_input("Please provide a string\n")
-ciphertext=""
-reverseplaintext=""
-alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-substitutionarray=['1','4','7','5','F','G','A','B','2','S','N','3','C','T','6','M','6','D','X','R','L','U','K','8','O','9']
+def encrypt(plaintext):
+	alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	substitutionarray=['1','4','7','5','F','G','A','B','2','S','N','3','C','T','6','M','6','D','X','R','L','U','K','8','O','9']
+	ciphertext=""
+	for i in plaintext:
+		ciphertext=ciphertext+substitutionarray[alphabet.index(i)]
+	return ciphertext
 
-for i in plaintext:
-	ciphertext=ciphertext+substitutionarray[alphabet.index(i)]
+def decrypt(ciphertext):
+	alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	substitutionarray=['1','4','7','5','F','G','A','B','2','S','N','3','C','T','6','M','6','D','X','R','L','U','K','8','O','9']
+	reverseplaintext=""
+	for i in ciphertext:
+		reverseplaintext=reverseplaintext+alphabet[substitutionarray.index(i)]
+	return reverseplaintext
 
-print "ciphertext is : " + ciphertext
+print encrypt("youcantbreakme")
+print decrypt(encrypt("youcantbreakme"))
 
-for i in ciphertext:
-	reverseplaintext=reverseplaintext+alphabet[substitutionarray.index(i)]
 
-print "reverseplaintext is : " + reverseplaintext
+
