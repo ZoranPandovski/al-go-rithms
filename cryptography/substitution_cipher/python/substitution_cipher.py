53e998014be9b95d60f9b64f4852217375c55b27
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-10-02 00:00:34 +0200

    Add docs and test for substitution cipher

--- cryptography/substitution_cipher/python/substitution_cipher.py
+++ cryptography/substitution_cipher/python/substitution_cipher.py
@@ -6,6 +6,7 @@ def encrypt(plaintext):
 		ciphertext=ciphertext+substitutionarray[alphabet.index(i)]
 	return ciphertext
 
+
 def decrypt(ciphertext):
 	alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 	substitutionarray=['1','4','7','5','F','G','A','B','2','S','N','3','C','T','6','M','6','D','X','R','L','U','K','8','O','9']
@@ -14,8 +15,15 @@ def decrypt(ciphertext):
 		reverseplaintext=reverseplaintext+alphabet[substitutionarray.index(i)]
 	return reverseplaintext
 
-print encrypt("youcantbreakme")
-print decrypt(encrypt("youcantbreakme"))
+
+def test():
+    cipher = encrypt("youcantbreakme")
+    plain_text = decrypt(cipher)
+    assert plain_text == "youcantbreakme"
+
+
+if __name__ == "__main__":
+    test()
 
 
 
