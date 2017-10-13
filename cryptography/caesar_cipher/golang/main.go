1a65d58a4ea5beff6ce0fd0a57ca49dbea9ea62f
gerifield <radics.gergely@ustream.tv>
2017-10-13 14:38:40 +0200

    Revert "Revert "Added caesar in go""
    This reverts commit 4ee9f868d678fd0888dad9c291e3290d4b3d2607.

---
+++ cryptography/caesar_cipher/golang/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "strings"
+
+// Caesar cipher
+func Caesar(text string, s int) string {
+	coded := make([]byte, 0, len(text))
+	for i := range strings.ToLower(text) {
+		coded = append(coded, shift(text[i], s))
+	}
+	return string(coded)
+}
+
+func shift(r byte, shift int) byte {
+	if r == ' ' {
+		return ' '
+	}
+	c := int(r) + shift
+	if c > 'z' {
+		return byte(c - 26)
+	}
+	if c < 'a' {
+		return byte(c + 26)
+	}
+	return byte(c)
+}
+
+func main() {
+	fmt.Println(Caesar("test", 3))
+	fmt.Println(Caesar("whvw", -3))
+}
