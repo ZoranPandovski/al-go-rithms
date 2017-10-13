33699aaccff1fec7923a005db392d2dbc57dbb38
gerifield <radics.gergely@ustream.tv>
2017-10-13 14:50:41 +0200

    Moved the search into a different file

--- search/linear_search/golang/main.go
+++ search/linear_search/golang/main.go
@@ -1,10 +1,6 @@
 package main
 
-import "errors"
 import "fmt"
-
-// ErrNotFound is the value when the item is not in the list
-var ErrNotFound = errors.New("not found")
 
 func main() {
 	nums := []int{3, 4, 5, 66, 42, 443, 4, 54, 6}
@@ -16,15 +12,4 @@ func main() {
 	}
 
 	fmt.Printf("%d found on index: %d\n", item, idx)
-}
-
-// LinearSearch sequentially checks each element of a list until
-// the correct element is found or until all elements have been searched
-func LinearSearch(nums []int, x int) (int, error) {
-	for i, num := range nums {
-		if num == x {
-			return i, nil
-		}
-	}
-	return 0, ErrNotFound
 }
