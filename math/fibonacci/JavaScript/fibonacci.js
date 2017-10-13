8aff26754691112cbe3b730f829e50597da972b4
mattgd <mattdzwonczyk@gmail.com>
2017-10-13 18:50:04 -0400

    Add explanation of Fibonacci for JavaScript.

--- math/fibonacci/JavaScript/fibonacci.js
+++ math/fibonacci/JavaScript/fibonacci.js
@@ -1,6 +1,13 @@
+/**
+ * Finds the series of n numbers in which each
+ * number is the sum of the two preceding numbers.
+ * @param {integer} n The number of iterations of Fibonacci
+ */
 function fibonacci(n) {
+    // Define base case
 	var fib = [1, 1];
-	
+    
+    // Iterate through using the sum of the last two values
 	for (i = 2; i < n; i++) {
         fib.push(fib[i - 1] + fib[i - 2]);
     }
