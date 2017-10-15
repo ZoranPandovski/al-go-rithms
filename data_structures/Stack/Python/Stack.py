1f33bd6d4b4b94c7353263585078536cb852252a
Banks <alsb@itu.dk>
2017-10-15 20:49:33 +0200

    Add Stack class and constructor

---
+++ data_structures/Stack/Python/Stack.py
@@ -0,0 +1,8 @@
+# Author: AlexBanks97
+# Purpose: LIFO Stack implementation using python array.
+# Date: October 15th 2017
+
+class Stack(object):
+    def __init__(self):
+        # Initialize stack as empty array
+        self.stack = [] 
