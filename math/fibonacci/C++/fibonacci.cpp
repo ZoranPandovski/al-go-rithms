33aef83852857c002f33b7bee2aeb30b04f3947f
Allen Computer Science Club <taylor_hudson@allenisd.org>
2017-10-20 06:26:30 -0500

    Update fibonacci.cpp
    Large Fibonacci Numbers Tend to degrade most data types with speed and regularity. BigInteger is the way most things are handled, but a simple application of BigInt using a string in C++ is a great way to promote understanding, and further arrays understanding.

--- math/fibonacci/C++/fibonacci.cpp
+++ math/fibonacci/C++/fibonacci.cpp
@@ -1,4 +1,53 @@
+// MOD AUTHOR:  Taylor Hudson
+// GITHUB    :  https://github.com/allencompsci
+// DATE      :  October 20, 2017
+// DESCRITION:  This C++ program implements the factorial operation in a space efficient, non-recursive manner and works beyond long long
+
+
 #include <iostream>
+#include <string>
+
+std::string add(std::string a, std::string b) { // ADDED 
+	std::string temp = "";
+	while ((int)a.length() < (int)b.length()) {
+		a = "0" + a;
+	}
+	while ((int)b.length() < (int)a.length()) {
+		b = "0" + b;
+	}
+	int carry = 0;
+	for (int i = a.length() - 1; i >= 0; i--) {
+		char val = (char)(((a[i] - 48) + (b[i] - 48)) + 48 + carry);
+		if (val > 57) {
+			carry = 1;
+			val -= 10;
+		}
+		else {
+			carry = 0;
+		}
+		temp = val + temp;
+	}
+	if (carry != 0) {
+		temp = "1" + temp;
+	}
+	while (temp[0] == '0' && temp.length() != 1) {
+		temp = temp.substr(1);
+	}
+	return temp;
+}
+
+void fib_Accurate(long long n) { // ADDED
+	std::string tmp = "";
+	std::string fibMinus1 = "1";
+	std::string fibMinus2 = "0";
+	std::string comma = ", ";
+		for (long long i = 0; i < n; i++) {
+			tmp = add(fibMinus1, fibMinus2);
+			std::cout << comma <<  fibMinus2;
+			fibMinus2 = fibMinus1;
+			fibMinus1 = tmp;
+	}
+}
 
 void fib(int n, int a, int b) {
 	int tmp;
@@ -11,11 +60,12 @@ void fib(int n, int a, int b) {
 }
 
 int main() {
-	int n;
+	long long n; // MODIFIED
 	int a = 0, b = 1;
 	std::cout << "How many numbers would you like to compute?\n>> ";
 	std::cin >> n;
 
-	fib(n, a, b);
+	fib((int)n, a, b); // MODIFIED
+	fib_Accurate(n); // ADDED
 	return 0;
 }
