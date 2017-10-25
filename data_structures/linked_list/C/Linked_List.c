202e0ca3b81eb86a94a2dfbae71c4b158efbf0ba
chirayu jain <chirayu.jain98@gmail.com>
2017-10-25 23:53:57 +0530

    detect_loop

--- data_structures/linked_list/C/Linked_List.c
+++ data_structures/linked_list/C/Linked_List.c
@@ -507,6 +507,63 @@ struct Node* addTwoLists (struct Node* first, struct Node* second)
     // return head of the resultant list
     return res;
 }
+int detectAndRemoveLoop(struct Node *list)
+{
+    struct Node  *slow_p = list, *fast_p = list;
+ 
+    while (slow_p && fast_p && fast_p->next)
+    {
+        slow_p = slow_p->next;
+        fast_p  = fast_p->next->next;
+ 
+        /* If slow_p and fast_p meet at some point then there
+           is a loop */
+        if (slow_p == fast_p)
+        {
+            removeLoop(slow_p, list);
+ 
+            /* Return 1 to indicate that loop is found */
+            return 1;
+        }
+    }
+ 
+    /* Return 0 to indeciate that ther is no loop*/
+    return 0;
+}
+ 
+/* Function to remove loop.
+ loop_node --> Pointer to one of the loop nodes
+ head -->  Pointer to the start node of the linked list */
+void removeLoop(struct Node *loop_node, struct Node *head)
+{
+   struct Node *ptr1;
+   struct Node *ptr2;
+ 
+   /* Set a pointer to the beging of the Linked List and
+      move it one by one to find the first node which is
+      part of the Linked List */
+   ptr1 = head;
+   while (1)
+   {
+     /* Now start a pointer from loop_node and check if it ever
+       reaches ptr2 */
+     ptr2 = loop_node;
+     while (ptr2->next != loop_node && ptr2->next != ptr1)
+         ptr2 = ptr2->next;
+ 
+     /* If ptr2 reahced ptr1 then there is a loop. So break the
+        loop */
+     if (ptr2->next == ptr1)
+        break;
+ 
+     /* If ptr2 did't reach ptr1 then try the next node after ptr1 */
+     ptr1 = ptr1->next;
+   }
+ 
+   /* After the end of loop ptr2 is the last node of the loop. So
+     make next of ptr2 as NULL */
+   ptr2->next = NULL;
+}
 int main()
 {
      struct node* a = NULL;
