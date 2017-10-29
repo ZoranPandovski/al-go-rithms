4eba5ca5f217ce22d63faec02d70834680820583
Anzoteh96 <anzoteh@hotmail.com>
2017-10-29 15:44:35 -0400

    Added Python version

---
+++ math/Prime_Factorization/Python/prime_factorization.py
@@ -0,0 +1,19 @@
+def prime_factorize(n):
+	if (n == 1):
+		return []
+	prime = 2
+	answer=[]
+	m = n
+	while (m > 1):
+		pwr = 0
+		while (m % prime == 0):
+			m /= prime
+			pwr = pwr + 1
+		if (pwr > 0):
+			temp=[prime, pwr]
+			answer.append(temp)
+		prime = prime + 1
+	return answer
+
+n = int(raw_input())
+print(prime_factorize(n))
