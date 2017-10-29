bf8abe18f7a2866c9820cd933279ff679f0e7d7b
Anzoteh96 <anzoteh@hotmail.com>
2017-10-29 16:10:31 -0400

    Added quick-unions, and updated readme

---
+++ graphsearch/connected_components/quick_union.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+//graph algorithms
+
+//first, the quick-find function
+
+int min(int a, int b) {
+	if (a <= b) {
+		return a;
+	}
+	return b;
+}
+
+int max(int a, int b) {
+	if (a >= b) {
+		return a;
+	}
+	return b;
+}
+
+map<int, int> quick_union(vector<vector<int> > edges) {
+	//result: connected components labelled with the connected components ID
+	//e.g. if (2 5 8) are in the same component and (4 9) are in the same component then (2 5 8) have ID 2 and (4 9) have ID 4
+	map<int, int>ccid; //connected component ID
+	map<int, int>parent;//the parent node
+	for (int i = 0; i < edges.size(); ++i) {
+		parent[edges[i][0]] = edges[i][0];
+		parent[edges[i][1]] = edges[i][1];
+	}
+	for (int i = 0; i < edges.size(); ++i) {
+		int node_0 = edges[i][0];
+		int node_1 = edges[i][1];
+		int root_0 = node_0;
+		int root_1 = node_1;
+		while (root_0 != parent[root_0]) {
+			root_0 = parent[root_0];
+		}
+		while (root_1 != parent[root_1]) {
+			root_1 = parent[root_1];
+		}
+		if (root_0 == root_1) {
+			continue;
+		}
+		parent[root_1] = root_0;
+	}
+	for (auto it = parent.begin(); it != parent.end(); ++it) {
+		int node = it->first;
+		int root = it->first;
+		while (root != parent[root]) {
+			root = parent[root];
+		}
+		ccid[node] = root;
+	}
+	return ccid;
+}
+
+int main() {
+	vector<vector<int> >edges(0, vector<int>(2));
+	int node1, node2;
+	while (cin >> node1 >> node2) {
+		vector<int>temp(2);
+		temp[0] = node1;
+		temp[1] = node2;
+		edges.push_back(temp);
+	}
+	map<int, int>ccid = quick_union(edges);
+	for (auto it = ccid.begin(); it != ccid.end(); ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+}
+
