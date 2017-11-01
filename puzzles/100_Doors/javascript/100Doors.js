b3a6b0bc25fd792c1985ba2c2e044c2d52151b9d
Ranjith Kumar Nagella <nagella.ranjith@gmail.com>
2017-10-31 22:47:18 -0700

    Update 100Doors.js
    update comment

--- puzzles/100_Doors/javascript/100Doors.js
+++ puzzles/100_Doors/javascript/100Doors.js
@@ -1,11 +1,10 @@
 const OPEN = 0;
 const CLOSE = 1;
-const numberOfDoors = 101; // since index starts with 1
+const numberOfDoors = 101; // since index going to be starting with 1
 let door = Array(numberOfDoors).fill(CLOSE, 1, numberOfDoors); // fill initla state with CLOSE
 for(walk = 1; walk < numberOfDoors; walk++) {
     door.map((value, index) => {
-        // first walk
-        if(walk === 1) {
+        if(walk === 1) { // first walk
             door[index] === OPEN ? door[index] = CLOSE: door[index] = OPEN;
         } else if ((index) % walk === 0) {
             door[index] === OPEN ? door[index] = CLOSE: door[index] = OPEN;
