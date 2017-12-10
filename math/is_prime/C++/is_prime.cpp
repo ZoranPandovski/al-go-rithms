ZoranPandovski/al-go-rithms

commit 8f80551a60877c488ca956c99743a146a997b7ff

To calculate fastly
2&3 are also prime

math/is_prime/C++/is_prime.cpp
@@ -7,7 +7,7 @@ constexpr bool isPrimeRecursive(const unsigned int number, const unsigned int re
 
 constexpr bool isPrime(const unsigned int number)
 {
-    return (number < 2) ? false : isPrimeRecursive(number, 2);
+    return (number < 4) ? false : isPrimeRecursive(number, 2);
 }
 
 int main()

commit 76c551d131a3f0373f0d39da40583800c40711d2

Fix bug introduced in 8f80551a60877c488ca956c99743a146a997b7ff

math/is_prime/C++/is_prime.cpp
@@ -7,7 +7,7 @@ constexpr bool isPrimeRecursive(const unsigned int number, const unsigned int re
 
 constexpr bool isPrime(const unsigned int number)
 {
-    return (number < 4) ? false : isPrimeRecursive(number, 2);
+    return (number < 2) ? false : isPrimeRecursive(number, 2);
 }
 
 int main()