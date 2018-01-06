5daf804f80606976cb27629cc69555809d6498e2
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-01-06 20:45:13 +0100

    Combine activity selection files

--- greedy/activity_selection/cpp/activity.cpp
+++ greedy/activity_selection/cpp/activity.cpp
@@ -1,30 +1,64 @@
-#include <bits/stdc++.h>
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(NULL);
-#define mp make_pair
-#define pb push_back
-#define inf_ll 2000000000000000000LL
-#define inf 1000000000
-#define eps 1e-8
-#define mod 1000000007
-#define ff first
-#define ss second
-#define N 3456789
+#include<stdio.h>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<utility>
+#include<algorithm>
+#include<cstdlib>
+#include<cstring>
+#include<string>
 using namespace std;
-typedef long long int ll;
-typedef pair <ll, ll> PII;
-PII ar[N];
+#define lli long long int
+#define mp make_pair
+#define f first
+#define s second
+#define sc(n) scanf("%d",&n);
+#define scl(n) scanf("%lld",&n);
+#define pr(n) printf("%d",n);
+#define prl(n) printf("%lld",n);
+#define nl printf("\n");
+#define all(c) (c).begin(), (c).end()
+#define sz(a) int((a).size())
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+void optimizeIO()
+{
+	ios_base:: sync_with_stdio(false);
+	cin.tie(NULL);
+}
+bool sortinasc(const pair<int,int> &a, const pair<int,int> &b)
+{
+	return (a.s < b.s);
+}
 
-int main () {
-	ll n, i, res = 1, j = 0;
+vector<pair<int, int> >arr;
+int main()
+{
+	int n;
 	cin >> n;
-	for (i = 0; i < n; i++) cin >> ar[i].ss >> ar[i].ff;
-	sort(ar, ar + n);	// Sort according to the finish times
-	for (i = 1; i < n; i++) {
-		if (ar[i].ss >= ar[j].ff) {			// If an activity does not conflict add it to res
-			res++;
+	int  t=n;
+	while(t--)
+	{
+		int u,v;
+		cin >> u >> v;
+		arr.push_back(mp(u,v));
+	}
+
+	sort(arr.begin(),arr.end(),sortinasc);
+//	tr(arr,it)
+//		cout << it->first << " " << it->second << endl;
+	int cnt = 1;
+	int j=0;
+	for(int i=1;i<n;i++)
+		if(arr[i].f >=  arr[j].s)
+		{
+			cnt ++;
 			j = i;
 		}
-	}
-	cout << res << "\n";
-	return 0;
+
+	cout << cnt;
+	nl;
 }
