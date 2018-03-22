60e5b53f13ac8c8901b29d569fa889a54111921a
suyash0103 <suyashghuge@gmail.com>
2018-03-22 19:19:33 +0530

    round robin cpp code

---
+++ process_scheduling/round robin/cpp/round_robin.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	printf("Enter number of processes:");
+	scanf("%d", &n);
+	int arrival[n];
+	int burst[n];
+	int completion[n];
+	int turn[n];
+	int waiting[n];
+	printf("Enter arrival and burst time of %d processes:\n", n);
+	queue <int> q;
+	for(int i = 0; i < n; i++)
+	{
+		int u, v;
+		scanf("%d%d", &u, &v);
+		// arrival.push_back(u);
+		// burst.push_back(v);
+		arrival[i] = u;
+		burst[i] = v;
+		q.push(i);
+	}
+	int tq;
+	printf("Enter time quantum:");
+	scanf("%d", &tq);
+	int completion_t = 0;
+	int first = q.front();
+	q.pop();
+	if(burst[first] <= tq)
+	{
+		// cout << "H";
+		completion[first] = burst[first];
+		completion_t = burst[first];
+		// cout << completion[first] << completion_t << endl;
+	}
+	else
+	{
+		q.push(first);
+		completion_t = tq;
+		burst[first] -= tq;
+	}
+	while(!q.empty())
+	{
+		int current = q.front();
+		q.pop();
+		if(burst[current] <= tq)
+		{
+			completion[current] = burst[current] + completion_t;
+			completion_t += burst[current];
+		}
+		else
+		{
+			q.push(current);
+			burst[current] -= tq;
+			completion_t += tq;
+		}
+	}
+	
+	for(int i = 0; i < n; i++)
+	{
+		turn[i] = completion[i] - arrival[i];
+		waiting[i] = turn[i] - burst[i];
+	}
+	for(int i = 0; i < n; i++)
+		printf("%d %d %d\n", completion[i], turn[i], waiting[i]);
+}
