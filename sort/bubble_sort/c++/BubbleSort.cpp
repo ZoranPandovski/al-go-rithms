4d5ed3e7e6f83c4a8ccc165d915eb45a107f1a6e
Chinmay-KB <chinmay.kabi@live.com>
2018-10-01 03:34:09 +0530

    Bucket Sort algorithm in python

--- sort/bubble_sort/c++/BubbleSort.cpp
+++ sort/bubble_sort/c++/BubbleSort.cpp
@@ -1,22 +1,21 @@
 #include<iostream>
 using namespace std;
-
-void bubble_sort( int A[ ], int n ) {
-    int temp;
-    for(int k = 0; k< n-1; k++) {
-        // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
-
-        for(int i = 0; i < n-k-1; i++) {
-            if(A[ i ] > A[ i+1] ) {
-                // here swapping of positions is being done.
-                temp = A[ i ];
-                A[ i ] = A[ i+1 ];
-                A[ i + 1] = temp;
+void bubble_sort(int*a, int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
 }
-
 int main()
 {
     int n;
