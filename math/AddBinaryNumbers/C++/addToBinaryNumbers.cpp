ddaddca3ef9adea27460a5c998c2e7a6a3c63fbd
varuncoc <43823353+varuncoc@users.noreply.github.com>
2018-10-03 22:45:22 +0530

    made changes
    it will noe be much better and easy./// 👍

--- math/AddBinaryNumbers/C++/addToBinaryNumbers.cpp
+++ math/AddBinaryNumbers/C++/addToBinaryNumbers.cpp
@@ -1,29 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
-
-string addBinNum(string a, string b)
-{
-	string result = "";
-	int sum = 0;
-
-	int i = a.size()-1, j = b.size()-1;
-	while (i>=0 || j>=0 || sum==1)
-	{
-		sum += ((i >= 0)? a[i]-48: 0);
-		sum += ((j >= 0)? b[j]-48: 0);
-		result = char(sum % 2 + 48) + result;
-		sum /= 2;
-		i--; 
-		j--;
-	}
-	return result;
-}
-
+ 
 int main()
 {
-	string a,b;
-	cout<<"Enter two binary numbers : ";
-	cin>>a>>b;
-	cout << addBinNum(a, b) << endl;
-	return 0;
-}
+	long bn1,bn2;
+	int i=0, r=0;
+	int sum[20]; 
+    cout << "\n\n Addition of two binary numbers:\n";
+	cout << "-----------------------------------\n";
+	cout << " Input the 1st binary number: ";
+	cin>> bn1;
+	cout << " Input the 2nd binary number: ";
+	cin>> bn2;
+  while (bn1 != 0 || bn2 != 0) 
+  {
+   sum[i++] = (int)((bn1 % 10 + bn2 % 10 + r) % 2);
+   r = (int)((bn1 % 10 + bn2 % 10 + r) / 2);
+   bn1 = bn1 / 10;
+   bn2 = bn2 / 10;
+  }
+  if (r != 0) {
+   sum[i++] = r;
+  }
+  --i;
+  cout<<" The sum of two binary numbers is: ";
+  while (i >= 0) {
+   cout<<(sum[i--]);
+  }
+   cout<<("\n");  
+ }  
