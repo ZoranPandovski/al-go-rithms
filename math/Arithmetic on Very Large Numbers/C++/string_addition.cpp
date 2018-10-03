8480e2f275605843ee8aae70037427c0611db7c9
varuncoc <43823353+varuncoc@users.noreply.github.com>
2018-10-03 22:54:33 +0530

    made beautiful awesome change
    it's now better than earlier.../ : D

--- math/Arithmetic on Very Large Numbers/C++/string_addition.cpp
+++ math/Arithmetic on Very Large Numbers/C++/string_addition.cpp
@@ -1,27 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<iostream.h>
+#include<string.h>
+#include<conio.h>
+#include<stdio.h>
 
-string strAdd(string s, string r){
-	int re=0;
-	string digit;
-	if(r.length()<s.length()) r.insert(r.begin(),s.length()-r.length(),'0');
-	else if(r.length()>s.length()) s.insert(s.begin(),r.length()-s.length(),'0');
-	for(int i=s.length()-1; i>=0;--i){
-		int a=(int(s[i]+r[i])+re-96);
-		digit.insert(digit.begin(),char(a%10+48));
-		re=a/10;
-	}
-	if(re!=0) digit.insert(digit.begin(),char(re+48));
-	return digit;
-}
-
-int main(){   
-	string s;
-	string c;
-	cout<<"Enter Number One"<<endl;
-	cin>>s;
-	cout<<"Enter Number Two"<<endl;
-	cin>>c;
-	cout<<"Addition Result: "<<strAdd(s,c)<<endl;
-	return 0;
+void main()
+{
+ clrscr();	
+char ch[100],ch1[100];
+	cout<<"\n Enter string 1: ";
+	gets(ch);
+	cout<<"\n Enter String 2: ";
+	gets(ch1);
+	cout<<"\n Merged string is: "<<strcat(ch,ch1);	
+getch();	
 }
