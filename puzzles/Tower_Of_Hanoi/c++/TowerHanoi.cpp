a1096c56a03f4e8638dd87fb10840cab0a18ce9e
Pushkar Patel <201851094@iiitvadodara.ac.in>
2018-10-04 21:18:24 +0530

    Updated TowerHanoi.cpp

--- puzzles/Tower_Of_Hanoi/c++/TowerHanoi.cpp
+++ puzzles/Tower_Of_Hanoi/c++/TowerHanoi.cpp
@@ -1,12 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void move(int n,char pole1,char pole2,char pole3){
+
+void move(int n,char pole1,char pole2,char pole3)
+{
 	if(n==0) return;
 	move(n-1,pole1,pole3,pole2);
 	cout<<n<<" "<<pole1<<" to "<<pole2<<endl;
 	move(n-1,pole3,pole2,pole1);
 }
-int main(){
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	int n;
 	cin>>n;
 	move(n,'a','b','c');
