fdc33a184bbca2428a079e2685bafe4c755d5629
jishnuvk <42906771+jishnuvk@users.noreply.github.com>
2018-10-04 23:01:29 +0530

    Update anagramcheck.h

--- puzzles/anagram/c++/anagramcheck.h
+++ puzzles/anagram/c++/anagramcheck.h
@@ -5,7 +5,7 @@ int isAnagram(char string1[],char string2[])
 {
 	char temp;
 
-	for(int i=0; string1[i]!=0; i++)
+	for(int i=0; string1[i]!=0; i++)          //this part arranges the letters of string 1 in alphabetical order 
 	{
 		for(int j=i+1; string1[j]!=0; j++)
 		{
@@ -19,7 +19,7 @@ int isAnagram(char string1[],char string2[])
 		}
 	}
 
-	for(int i=0; string2[i]!=0; i++)
+	for(int i=0; string2[i]!=0; i++)       //this part arranges the letters of string 2 in alphabetical order 
 	{
 		for(int j=i+1; string2[j]!=0; j++)
 		{
@@ -34,7 +34,7 @@ int isAnagram(char string1[],char string2[])
 	}
 
 
-	if(strcmp(string1,string2)==0)
+	if(strcmp(string1,string2)==0)  //if the alphabetically ordered strings are the same, the strings must be anagrams
 		return 1;
 	else 
 		return 0;
