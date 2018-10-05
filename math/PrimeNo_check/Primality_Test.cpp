3a48d9f48498f645553ea044f8c5da109bf51573
prateekyo <43875858+prateekyo@users.noreply.github.com>
2018-10-05 13:56:17 +0530

    done

--- math/PrimeNo_check/Primality_Test.cpp
+++ math/PrimeNo_check/Primality_Test.cpp
@@ -1,40 +1,92 @@
-#include <iostream>
-using namespace std;
- 
- 
- //A simple and efficient implementation of a function to test if a number is prime, based on the fact that
- //Every Prime number, except 2 and 3 are of the form 6*k+1 or 6*k-1 for integer values of k.
- //it checks if the given number is divisible by all numbers of the form 6k ± 1.
- 
- bool IsPrime( int number )
- {
-  if ( ( (!(number & 1)) && number != 2 ) || (number < 2) || (number % 3 == 0 && number != 3) )
-    {
-    return false;
-    }
-
-    for( int k = 1; 36*k*k-12*k < number;++k)
-    {
-   	if ( (number % (6*k+1) == 0) || (number % (6*k-1) == 0) )
-        {
-        return false;
-        }		
-  	}
-   return true;
- }
- 
-  	//Main Function
-    
- int main()
- {
- 	cout <<"Enter the value of n to check if Prime\n";
- 	int n;
- 	cin >> n;
+#include <bits/stdc++.h> 
+using namespace std; 
   
- 	if(IsPrime(n))
- 		cout << n << " is Prime" <<endl;
- 	else	
- 		cout << n << " is not Prime" <<endl;
-     
- 	return 0;
- }
+// Utility function to do modular exponentiation. 
+// It returns (x^y) % p 
+int power(int x, unsigned int y, int p) 
+{ 
+    int res = 1;      // Initialize result 
+    x = x % p;  // Update x if it is more than or 
+                // equal to p 
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+  
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    return res; 
+} 
+  
+// This function is called for all k trials. It returns 
+// false if n is composite and returns false if n is 
+// probably prime. 
+// d is an odd number such that  d*2<sup>r</sup> = n-1 
+// for some r >= 1 
+bool miillerTest(int d, int n) 
+{ 
+    // Pick a random number in [2..n-2] 
+    // Corner cases make sure that n > 4 
+    int a = 2 + rand() % (n - 4); 
+  
+    // Compute a^d % n 
+    int x = power(a, d, n); 
+  
+    if (x == 1  || x == n-1) 
+       return true; 
+  
+    // Keep squaring x while one of the following doesn't 
+    // happen 
+    // (i)   d does not reach n-1 
+    // (ii)  (x^2) % n is not 1 
+    // (iii) (x^2) % n is not n-1 
+    while (d != n-1) 
+    { 
+        x = (x * x) % n; 
+        d *= 2; 
+  
+        if (x == 1)      return false; 
+        if (x == n-1)    return true; 
+    } 
+  
+    // Return composite 
+    return false; 
+} 
+  
+// It returns false if n is composite and returns true if n 
+// is probably prime.  k is an input parameter that determines 
+// accuracy level. Higher value of k indicates more accuracy. 
+bool isPrime(int n, int k) 
+{ 
+    // Corner cases 
+    if (n <= 1 || n == 4)  return false; 
+    if (n <= 3) return true; 
+  
+    // Find r such that n = 2^d * r + 1 for some r >= 1 
+    int d = n - 1; 
+    while (d % 2 == 0) 
+        d /= 2; 
+  
+    // Iterate given nber of 'k' times 
+    for (int i = 0; i < k; i++) 
+         if (!miillerTest(d, n)) 
+              return false; 
+  
+    return true; 
+} 
+  
+// Driver program 
+int main() 
+{ 
+    int k = 4;  // Number of iterations 
+  
+    cout << "All primes smaller than 100: \n"; 
+    for (int n = 1; n < 100; n++) 
+       if (isPrime(n, k)) 
+          cout << n << " "; 
+  
+    return 0; 
+} 
