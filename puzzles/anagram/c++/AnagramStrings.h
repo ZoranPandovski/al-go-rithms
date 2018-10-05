ee85006a761725d4d1072e313ffb16fdc9badfec
zetamoo <zetamoo@gmail.com>
2018-10-05 15:10:20 +0200

    Fixing incorrect algorithm for anagram algorithm
    Issue: https://github.com/ZoranPandovski/al-go-rithms/issues/1148

--- puzzles/anagram/c++/AnagramStrings.h
+++ puzzles/anagram/c++/AnagramStrings.h
@@ -2,42 +2,47 @@
 
 #include <string>
 #include <algorithm>
+#include <assert.h>
 
-//Write a method to decide if two strings are anagrams or not.
-//Following assumptions are taken below:
-//1) Only alphabets are there in strings.
-//Using XOR property A^A = 0
+// Write a method to decide if two strings are anagrams or not.
+// Following assumptions are taken below:
+// 1- Only alphabets are there in strings.
+// 2- The alphabet consists of 26 letters, starting from 'a' to 'z'.
+// if a non-alphabet character is used the function throws an exception.
 
-bool isAnagrams(std::string str1, std::string str2){
+static const int ALPHABET_SIZE = 26;
+
+bool isAnagrams(std::string str1, std::string str2) {
 
 	if (str1.length() != str2.length())
 		return false;
+	
+	int strLength = str1.length();
 
-	int check1 = 0;
-
-	//Transform characters to lowercase
+	// Transform characters to lowercase
 	std::transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
-
-	//collecting str1 alphabets ascii value in check1
-	for (int i = 0; i < str1.length(); ++i){
-		int asciiVal = static_cast<int>(str1[i]) - 'a';
-
-		check1 |= (1 << asciiVal);
-	}
-
-	int check2 = 0;
-
-	//collecting str2 alphabets value in check2
 	std::transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
-	for (int i = 0; i < str2.length(); ++i){
-		int asciiVal = static_cast<int>(str2[i]) - 'a';
 
-		check2 |= (1 << asciiVal);
+	// The difference between frequency of each letter in str1 and str2
+	int frequencyDiff[ALPHABET_SIZE]={0};
+	
+	// Calculating the frequency of letters for str1 and str2
+	for (int i = 0; i < strLength; ++i) {
+		// str1
+		int charValue = static_cast<int>(str1[i]) - 'a';
+		assert(charValue >= 0 && charValue < ALPHABET_SIZE);
+		frequencyDiff[charDiff]++;
+
+		// str2
+		charValue = static_cast<int>(str2[i]) - 'a';
+		assert(charValue >= 0 && charValue < ALPHABET_SIZE);
+		frequencyDiff[charDiff]--;
 	}
 
-	//if both strings has same characters then A ^ A will result 0
-	if ((check1 ^ check2) == 0)
-		return true;
-
-	return false;
+	for (int i = 0; i < ALPHABET_SIZE; ++i) {
+		if (ALPHABET_SIZE[i] != 0) {
+			return false;
+		}
+	}
+	return true;
 }
