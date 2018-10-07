113bf8341c906f5d5fc05996b0ed63f6e4d379dc
Rathin Bhargava <30757416+Mindjolt2406@users.noreply.github.com>
2018-10-07 20:03:30 +0530

    Added a cpp version of BIT/Fenwick Tree

---
+++ data_structures/quadtree/c++/BIT.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#define mt make_tuple
+#define mp make_pair
+#define pu push_back
+#define INF 1000000001
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long int>
+#define sc(n) scanf("%d",&n);
+#define scll(n) scanf("%lld",&n);
+#define scld(n) scanf("%Lf",&n);
+#define scr(s) {char temp[1000000];scanf("%s",temp);s = temp;}
+
+using namespace std;
+
+struct BIT //int
+{
+  vector<int> bit;
+  int n;
+
+  void init(int n)
+  {
+    this->n = n;
+    bit.assign(n,0);
+  }
+
+  int sum(int i)
+  {
+    int res = 0;
+    for(;i>=0;i = (i&(i+1)) -1)
+    {
+      res+=bit[i];
+    }
+    return res;
+  }
+
+  void inc(int i, int delta)
+  {
+    for(;i<n;i = i|(i+1))
+    {
+      bit[i]+=delta;
+    }
+  }
+
+  int getsum(int l,int r)
+  {
+    // If l==0, sum(-1) automatically returns the default calue of res, 0
+    return sum(r)-sum(l-1);
+  }
+
+  void init(vector<int> v)
+  {
+    init(v.size());
+    for(int i=0;i<v.size();i++) inc(i,v[i]);
+  }
+};
+
+struct BIT //ll
+{
+  vector<ll> bit;
+  int n;
+
+  void init(int n)
+  {
+    this->n = n;
+    bit.assign(n,0);
+  }
+
+  ll sum(int i)
+  {
+    ll res = 0;
+    for(;i>=0;i = (i&(i+1)) -1)
+    {
+      res+=bit[i];
+    }
+    return res;
+  }
+
+  void inc(int i, ll delta)
+  {
+    for(;i<n;i = i|(i+1))
+    {
+      bit[i]+=delta;
+    }
+  }
+
+  ll getsum(int l,int r)
+  {
+    // If l==0, sum(-1) automatically returns the default calue of res, 0
+    return sum(r)-sum(l-1);
+  }
+
+  void init(vector<int> v)
+  {
+    init(v.size());
+    for(int i=0;i<v.size();i++) inc(i,v[i]);
+  }
+};
+
+int main()
+{
+  typedef struct BIT BIT;
+  int n;
+  sc(n);
+  vector<int> v;
+  for(int i=0;i<n;i++) {int a;sc(a);v.pu(a);}
+  BIT bit;
+  bit.init(v);
+  int q;
+  sc(q);
+  for(int h=0;h<q;h++)
+  {
+    cout<<bit.getsum(0,4)<<endl;
+    bit.inc(0,5);
+    cout<<bit.getsum(0,4)<<endl;
+  }
+  return 0;
+}
+
