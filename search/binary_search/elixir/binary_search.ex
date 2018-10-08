6713db0d35bbaa60c42fff36f7d7e0be94527d01
Theo Dowling <thdowling@gmail.com>
2018-10-08 16:00:50 +0200

    Some formatting updates

--- search/binary_search/elixir/binary_search.ex
+++ search/binary_search/elixir/binary_search.ex
@@ -9,16 +9,18 @@ defmodule Search do
     mid = Integer.floor_div(low + high, 2)
 
     cond do
-      Enum.at(array, mid) == num
-        -> true
-      Enum.at(array, mid) < num
-        -> binary_search(array, mid + 1, high, num)
-      true 
-        -> binary_search(array, low, mid - 1, num)
+      Enum.at(array, mid) == num ->
+        true
+
+      Enum.at(array, mid) < num ->
+        binary_search(array, mid + 1, high, num)
+
+      true ->
+        binary_search(array, low, mid - 1, num)
     end
   end
 
   defp binary_search(_array, low, high, _num) when low > high, do: false
 end
 
-IO.inspect Search.binary_search([4, 5, 9, 88, 104, 203, 501, 670], 5)
+IO.inspect(Search.binary_search([4, 5, 9, 88, 104, 203, 501, 670], 5))
