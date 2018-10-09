2abcbcc65028d828d3d6f6e0ccf79d6356807c06
taruncode <43909818+taruncode@users.noreply.github.com>
2018-10-09 20:48:39 +0530

    made changes

--- Array-Palindrom-C++/ArrayPalindrom.cpp
+++ Array-Palindrom-C++/ArrayPalindrom.cpp
@@ -1,79 +1,29 @@
 #include <iostream>
-#include <stdio.h>
 
+template <typename T>
+bool isPalindrome(T arrayToCheck[], unsigned int arraySize);
 
 using namespace std;
 
-int main(void)
-{
-    int  i,j,ii,jj,n;
-    int count=0, max_count=0, start_index=0, temp_start_index=0;
-    int data[100] ;
+int main() {
+  int arrayToCheck[6] = { 1, 2, 4, 4, 2, 1 };
 
+  cout << boolalpha << isPalindrome(arrayToCheck, 6);
 
+  int tmp;
+  cin >> tmp;
 
-cout<<"Input Array n : ";
-cin>>n;
-cout<<endl;
-for(int i=1;i<=n;i++)
-{
-
-cin>>data[i];
+  return 0;
 }
-cout<<endl;
-
-    for(int i=0; i < n+1; ++i)
-         {
-       for (j=n;j>=0;--j)
-       {
-          for (ii=i,jj=j;;++ii,--jj)
-          {
-             if (data[ii]==data[jj])
-             {
-                if (count==0)
-                {
-                   temp_start_index=ii;
-                }
-                ++count;
-                if (ii==jj)
-                {
-                    count=count*2-1;
-                    break;
-                }else{
-                       if (ii>jj)
-                       {
-                          --count;
-                          count=count*2;
-                          break;
-                       }
-               }
-
-           }
-
-           else
-           {
-               count=0;
-               break;
-           }
-       }
 
 
-       if (count>max_count)
-       {
-           max_count=count;
-           start_index=temp_start_index;
-           count=0;
-           temp_start_index=0;
-
-       }
-   }
- }
- cout<<"Subarray palindrom";
- cout<<endl;
-  for (i=start_index;i<start_index+max_count;++i)
-  {
-    printf("%d ",data[i]);
-
+template <typename T>
+bool isPalindrome(T arrayToCheck[], unsigned int arraySize) {
+  for (unsigned int i = 0; i < arraySize / 2; i++) {
+    if (arrayToCheck[i] != arrayToCheck[arraySize - 1 - i]) {
+      return false;
+    }
   }
- }
 
+  return true;
+}
