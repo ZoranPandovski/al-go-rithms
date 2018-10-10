ee448352f340917d2debf9035d239dc80388ed5c
tarun-codestien <44030453+tarun-codestien@users.noreply.github.com>
2018-10-10 21:12:55 +0530

    done
    changed with complete description

--- search/binary_search/C/binarySearch.c
+++ search/binary_search/C/binarySearch.c
@@ -1,32 +1,43 @@
-#include <stdio.h>
-
-int binarySearch(int arr[], int start, int end, int x) {
-
-	if (start <= end) {
-
-		int mid = start + (end - start) / 2;
-
-		if (arr[mid] == x)
-			return mid;
-
-		if (arr[mid] > x)
-			return binarySearch(arr, start, mid - 1, x);
-
-		return binarySearch(arr, mid + 1, end, x);
-	}
-	return -1;
-}
-
-int main()
-{
-	int arr[] = { 2, 3, 7, 8, 78, 99, 102, 5555 };
-	int x = 9;
-	int result = binarySearch(arr, 0, sizeof(arr) / sizeof(arr[0]), x);
-
-	if (result == -1) {
-		printf("Element not present");
-	}
-	else {
-		printf("Element found at index %d", result);
-	}
-}
+// C program to implement recursive Binary Search 
+#include <stdio.h> 
+  
+// A recursive binary search function. It returns  
+// location of x in given array arr[l..r] is present,  
+// otherwise -1 
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+   if (r >= l) 
+   { 
+        int mid = l + (r - l)/2; 
+  
+        // If the element is present at the middle  
+        // itself 
+        if (arr[mid] == x)   
+            return mid; 
+  
+        // If element is smaller than mid, then  
+        // it can only be present in left subarray 
+        if (arr[mid] > x)  
+            return binarySearch(arr, l, mid-1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch(arr, mid+1, r, x); 
+   } 
+  
+   // We reach here when element is not  
+   // present in array 
+   return -1; 
+} 
+  
+int main(void) 
+{ 
+   int arr[] = {2, 3, 4, 10, 40}; 
+   int n = sizeof(arr)/ sizeof(arr[0]); 
+   int x = 10; 
+   int result = binarySearch(arr, 0, n-1, x); 
+   (result == -1)? printf("Element is not present in array") 
+                 : printf("Element is present at index %d", 
+                                                   result); 
+   return 0; 
+} 
