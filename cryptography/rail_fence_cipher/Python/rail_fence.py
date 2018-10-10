4de1035d0b73cd403850fc4a81e61e88dbb6faa3
Pratok-Sanghani <pratiksanghani70@gmail.com>
2018-10-10 23:39:07 +0530

    Rail_fence in python.

---
+++ cryptography/rail_fence_cipher/Python/rail_fence.py
@@ -0,0 +1,24 @@
+def main():
+    layers = int(input("Enter the number of layers: "))
+    plain_text = input("Enter the plain text: ")
+    cipher_text = encrypt(layers, plain_text)
+    print("Encrypted text: " + cipher_text)
+
+
+def encrypt(layers, plain_text):
+    plain_text = plain_text.replace(" ", "")
+    plain_text = plain_text.upper()
+    rail = [""] * layers
+    layer = 0
+    for character in plain_text:
+        rail[layer] += character
+        if layer >= layers - 1:
+            layer = 0
+        else:
+            layer += 1
+
+    cipher = "".join(rail)
+    return cipher
+
+if __name__ == '__main__':
+    main()
