3e17597c4d5992f0aaa951a4c1fa8dbc4a6a1379
tarun-codestien <44030453+tarun-codestien@users.noreply.github.com>
2018-10-10 21:21:16 +0530

    done1

--- search/binary_search/c++/Binary_Search.cpp
+++ search/binary_search/c++/Binary_Search.cpp
@@ -1,105 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
-bool binarySearchIterative(int list[], int first, int end, int item)
-{
-
-	int middle;
-
-	while(first <= end)
-	{
-	     middle =first + (end- first)/2;
-	     if(list[middle] == item)
-	        {
-			return true;
-		}
-		if(list[middle] < item)
-		{
-			first = middle + 1;
-		} else
-		{
-			end = middle - 1;
-		}
-	}
-	return false;
-}
-
-bool binarySearchRecursive(int list[], int first, int end, int item)
-{
-
-	int middle = (first + end)/2;
-
-	if(list[middle] == item)
-	{
-		return true;
-	}
-	if(first >= end)
-	{
-		return false;
-	}
-	if(list[middle] < item)
-	{
-		return binarySearchRecursive(list, middle+1, end, item);
-	} else
-	{
-		return binarySearchRecursive(list, first, middle-1, item);
-	}
-
-}
-
-int main(int argc, char const *argv[])
-{
- cout << "Please, enter the size of the list." << endl;
-  int size, value, option;
-  bool find;
- cin >> size;
-  int *list = new int[size];
-  for(int i = 0; i < size; i++)
-  {
-   cout << "Enter the element " << i << " of the list." << endl;
-   cin >> list[i];
-  }
- cout << "Now, enter a value to be searched in the list." << endl;
- cin >> value;
-  cout << "Now, would you like to use a binary search iterative (0) or a binary search recursive(1)." <<endl;
- cin >> option;
-  switch (option) {
-    case 0:
-      {
-        auto start =chrono::steady_clock::now();
-        find = binarySearchIterative(list, 0, size-1, value);
-        auto end =chrono::steady_clock::now();
-        auto diff = end - start;
-        if(find)
-        {
-         cout << "You find the element using the binary search iterative." << endl;
-        }
-        else{
-          cout << "You not find the element using the binary search iterative." <<endl;
-        }
-       cout << "And the time of search was " <<chrono::duration <double,milli> (diff).count() << " ms." << endl;
-      }
-      break;
-    case 1:
-      {
-        auto start =chrono::steady_clock::now();
-        find = binarySearchRecursive(list, 0, size-1, value);
-        auto end =chrono::steady_clock::now();
-        auto diff = end - start;
-        if(find)
-        {
-          cout << "You find the element using the binary search recursive." <<endl;
-        }
-        else
-	{
-         cout << "You not find the element using the binary search recursive." << endl;
-        }
-       cout << "And the time of search was " << chrono::duration <double, milli> (diff).count() << " ms." << endl;
-      }
-      break;
-    default:
-      cout << "This isn't a valid option, please restart the program." << endl;
-      break;
-  }
-  delete[] list;
-	return 0;
-}
+// C program to implement recursive Binary Search 
+#include <stdio.h> 
+  
+// A recursive binary search function. It returns  
+// location of x in given array arr[l..r] is present,  
+// otherwise -1 
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+   if (r >= l) 
+   { 
+        int mid = l + (r - l)/2; 
+  
+        // If the element is present at the middle  
+        // itself 
+        if (arr[mid] == x)   
+            return mid; 
+  
+        // If element is smaller than mid, then  
+        // it can only be present in left subarray 
+        if (arr[mid] > x)  
+            return binarySearch(arr, l, mid-1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch(arr, mid+1, r, x); 
+   } 
+  
+   // We reach here when element is not  
+   // present in array 
+   return -1; 
+} 
+  
+int main(void) 
+{ 
+   int arr[] = {2, 3, 4, 10, 40}; 
+   int n = sizeof(arr)/ sizeof(arr[0]); 
+   int x = 10; 
+   int result = binarySearch(arr, 0, n-1, x); 
+   (result == -1)? printf("Element is not present in array") 
+                 : printf("Element is present at index %d", 
+                                                   result); 
+   return 0; 
+} 
