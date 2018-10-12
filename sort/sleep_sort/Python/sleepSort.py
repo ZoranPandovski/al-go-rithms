caba32a52b785b8bde5ba32ae13dc9d40761e71e
Chris Doucette <32644087+ediblesushi@users.noreply.github.com>
2018-10-12 09:24:30 -0400

    Added test to sleepSort.py
    Added test to sleepSort.py and changed filename from sleeSort.py to sleepSort.py

--- sort/sleep_sort/Python/sleepSort.py
+++ sort/sleep_sort/Python/sleepSort.py
@@ -18,5 +18,11 @@ def main():
     v = [5,2,6,1,4,3]
     sleepSort(v);
 
+def test():
+    list = [4,2,6,3,1,5]
+    sorted_list = sleepSort(list)
+    assert (sorted(list) == sorted_list)
+    
 if __name__ == "__main__":
+    test()
     main()
