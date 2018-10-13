1e7176484d41769d04d46b2f8877764a87e17851
Saifuddin Syed <23101926+saifserver@users.noreply.github.com>
2018-10-13 14:15:39 +0530

    Add condition for one bottle
    It will now print -
    "2 bottles of beer on the wall, 2 bottles of beer.
    Take one down and pass it around, 1 bottle of beer on the wall.
    1 bottle of beer on the wall, 1 bottle of beer."
    instead of -
    "2 bottles of beer on the wall, 2 bottles of beer.
    Take one down and pass it around, 1 bottle(s) of beer on the wall.
    1 bottle(s) of beer on the wall, 1 bottle(s) of beer."

--- puzzles/99_Bottles/javascript/99bottles.js
+++ puzzles/99_Bottles/javascript/99bottles.js
@@ -1,9 +1,21 @@
 let bottles = 99;
 while (bottles != 0)
 {
-  console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`);
-  --bottles;
-  console.log(`Take one down and pass it around, ${bottles} bottles of beer on the wall.`);
+	if (bottles === 1) {
+		console.log(`${bottles} bottle of beer on the wall, ${bottles} bottles of beer.`);
+	} else {
+		console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`);
+	}
+	
+	--bottles;
+	
+	if (bottles === 1) {
+		console.log(`Take one down and pass it around, ${bottles} bottle of beer on the wall.`);
+	} else if (bottles === 0) {
+		console.log(`Take one down and pass it around, no more bottles of beer on the wall.`);
+	} else {
+		console.log(`Take one down and pass it around, ${bottles} bottles of beer on the wall.`);
+	}
 }
 console.log('No more bottles of beer on the wall, no more bottles of beer.');
 console.log('Go to the store and buy some more, 99 bottles of beer on the wall.');
