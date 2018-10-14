93b079ba5f2343c01a04e8a830153bcabf119fbc
Berf Uğurlu <44122537+kruvatz@users.noreply.github.com>
2018-10-14 04:13:16 +0300

    Create arithmeticpattern.py

---
+++ math/arithmeticpattern.py
@@ -0,0 +1,17 @@
+n1= input("Enter first term of pattern")
+
+nform = input("Enter increase amount")
+
+n = input("How many steps to go?")
+
+
+ary = []
+
+for x in range(n):
+    ary.append((x)*nform + n1)
+
+
+
+
+
+print ary
