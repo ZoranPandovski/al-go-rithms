f7fbe3bcafc14decfb3124a342ed7d8538949312
Rohan <rohan_daxini@yahoo.com>
2018-10-15 09:59:58 +0530

    add more examples with random array and Benchmark it

--- sort/radix_sort/ruby/radix_sort.rb
+++ sort/radix_sort/ruby/radix_sort.rb
@@ -13,4 +13,16 @@ class Array
   end
 end
 
+# Perform radix sort on a fixed array
 p [210, 52, 30, -100, 3, 21, -750, 18, -12].radix_sort
+
+# Perform radix sort on a randomly generated array
+p Array.new(10) { rand(-500...500) }.radix_sort
+
+# Benchmark radix sort
+require 'benchmark'
+puts Benchmark.measure {
+  100_000.times do
+    Array.new(10) { rand(-500...500) }.radix_sort
+  end
+}
