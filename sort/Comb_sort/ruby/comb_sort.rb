21d10fcf283130f4e5c9e5f848763d56630f3f87
Rohan <rohan_daxini@yahoo.com>
2018-10-15 14:28:09 +0530

    dont need print statements

--- sort/Comb_sort/ruby/comb_sort.rb
+++ sort/Comb_sort/ruby/comb_sort.rb
@@ -18,10 +18,10 @@ class Array
 end
 
 # Perform comb sort on a fixed array
-p [3, 21, 75, 47, 93, 30, 3, 9, 1, 51, 12, 14, 12, 57, 78].combsort!
+[3, 21, 75, 47, 93, 30, 3, 9, 1, 51, 12, 14, 12, 57, 78].combsort!
 
 # Perform comb sort on a randomly generated array
-p Array.new(10) { rand(-500...500) }.combsort!
+Array.new(10) { rand(-500...500) }.combsort!
 
 # Benchmark comb sort
 require 'benchmark'
