4c33cf0083a625f7a5816e7cf76ae566007d087c
Patrick Biffle <pbiffle@gmail.com>
2018-10-17 14:22:29 -0600

    added anagram finding function in javascript and cleaned up isogram finding function javascript

--- strings/string_fundamental/javascript/isogram.js
+++ strings/string_fundamental/javascript/isogram.js
@@ -1,8 +1,8 @@
 const isIsogram = (word) => {
-    if(word.length === 0) return true
+    if (word.length === 0) return true
     word = word.toLowerCase()
     let letters = {}
-    for(let i = 0; i < word.length; i++){
+    for (let i = 0; i < word.length; i++) {
       if (!letters[word[i]]) {
         letters[word[i]] = 1 
       }
