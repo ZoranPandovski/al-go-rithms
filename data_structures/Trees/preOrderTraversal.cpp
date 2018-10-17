f7155a99ff575b9508a27b2fd9d3d597f14d8024
bahl24 <36095178+bahl24@users.noreply.github.com>
2018-10-17 08:23:03 +0530

    preOrderTraversal.cpp

---
+++ data_structures/Trees/preOrderTraversal.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstddef>
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/* The function preOrder contains the algorithm for pre order traversal of a tree. The address of the root node is passed as a parameter*/
+
+        void preOrder(Node *root) {
+        std::cout<<root->data<<" ";
+        if(root->left!=NULL)
+            preOrder(root->left);
+        if(root->right!=NULL)
+            preOrder(root->right);
+        
+    }
+ };
+ 
+ int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.preOrder(root);
+
+    return 0;
+}
