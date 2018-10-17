0d4789c07e156428d35c8d3b29d49113688d5777
Patrick Biffle <pbiffle@gmail.com>
2018-10-17 13:52:32 -0600

    added isogram in javascript

---
+++ strings/string_fundamental/javascript/isogram.js
@@ -0,0 +1,12 @@
+const isIsogram = (word) => {
+    if(word.length === 0) return true
+    word = word.toLowerCase()
+    let letters = {}
+    for(let i = 0; i < word.length; i++){
+      if (!letters[word[i]]) {
+        letters[word[i]] = 1 
+      }
+      else return false
+    }
+    return true
+  }
