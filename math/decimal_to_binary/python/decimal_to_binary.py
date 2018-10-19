99e4b882c22844c99e6dae3b473b93628fa7086b
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-10-19 19:44:33 +0530

    Update and rename decimal_to_binary_1.py to decimal_to_binary.py

--- math/decimal_to_binary/python/decimal_to_binary.py
+++ math/decimal_to_binary/python/decimal_to_binary.py
@@ -2,7 +2,6 @@ def decimal_to_binary(n):
     if n > 1: decimal_to_binary(n//2)
     print(n % 2, end = '')
 
-try: num = int(input("Enter an Integer to Convert into Binary: "))
+try: decimal_to_binary(int(input("Enter an Integer to covert to Binary: ")))
 except ValueError: print("Input is not an integer.")
-decimal_to_binary(num)
 print()
