1ea29a954a92885809a6162bc21bb321d4c276bf
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-19 16:21:14 +0530

    Update is_prime.js

--- math/is_prime/javascript/is_prime.js
+++ math/is_prime/javascript/is_prime.js
@@ -11,7 +11,7 @@
  * Is not efficient for very large prime numbers
  */
 
-const isPrime = (n) => {
+const isPrime = n => {
     if (n <= 1) return false;
     else if (n <= 3) return true;
     else if (n % 2 === 0 || n % 3 === 0) return false
