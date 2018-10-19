52afccec8bdb9f48c33d6affd3e42d5d35027484
Aniruddha Pandey <anirudh.pandev@gmail.com>
2018-10-19 19:45:18 +0530

    Update decimal_to_binary.py

--- math/decimal_to_binary/python/decimal_to_binary.py
+++ math/decimal_to_binary/python/decimal_to_binary.py
@@ -2,6 +2,6 @@ def decimal_to_binary(n):
     if n > 1: decimal_to_binary(n//2)
     print(n % 2, end = '')
 
-try: decimal_to_binary(int(input("Enter an Integer to covert to Binary: ")))
+try: decimal_to_binary(int(input("Enter an Integer to Covert into Binary: ")))
 except ValueError: print("Input is not an integer.")
 print()
