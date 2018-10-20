fe3a36a5818749a18be1c3068d621e040641a836
Rafael Monteiro <rafael.monteiro@theiconic.com.au>
2018-10-20 21:31:26 +1100

    Add extra line and algorithm description

--- strings/string_fundamental/php/palindrome.php
+++ strings/string_fundamental/php/palindrome.php
@@ -1,5 +1,9 @@
 <?php
 
+/* 
+ * A palindrome is a word, phrase, or sequence that reads the same 
+ * backward as forward, e.g., madam or nurses run.
+ */
 class Palindrome
 {
     public static function isPalindrome($word)
