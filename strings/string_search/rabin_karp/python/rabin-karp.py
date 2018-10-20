693bae5ff4a93a312e35a605fa8158194ab20398
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2018-10-20 23:23:44 +0530

    Updated Rabin Karp.py

--- strings/string_search/rabin_karp/python/rabin-karp.py
+++ strings/string_search/rabin_karp/python/rabin-karp.py
@@ -1,43 +1,60 @@
-# Function requires the following arguments:
-# text,
-# pattern,
-# d = number of characters in input alphabet,
-# q = a prime number
-
-
-def rabin_karp_algorithm(text, pattern, d, q):
-    n = len(text)
-    m = len(pattern)
-    h = pow(d, m-1) % q
-    p = 0                   # hash value of the pattern
-    t = 0                   # hash value of the text
-    result = []
-    # calculate the hash values for pattern and text
-    for i in range(m):
-        p = (d * p + ord(pattern[i])) % q
-        t = (d * t + ord(text[i])) % q
-
-    # Slide pattern over text
-    for s in range(n - m + 1):
-        # Check the hash values of the current window of text and pattern
-        if p == t:
-            # Check for characters one by one
-            match = True
-            for i in range(m):
-                if pattern[i] != text[s+i]:
-                    match = False
+# Following program is the python implementation of 
+# Rabin Karp Algorithm 
+  
+# d is the number of characters in the input alphabet 
+d = 256
+  
+# pat  -> pattern 
+# txt  -> text 
+# q    -> A prime number 
+  
+def search(pat, txt, q): 
+    M = len(pat) 
+    N = len(txt) 
+    i = 0
+    j = 0
+    p = 0    # hash value for pattern 
+    t = 0    # hash value for txt 
+    h = 1
+  
+    # The value of h would be "pow(d, M-1)%q" 
+    for i in xrange(M-1): 
+        h = (h*d)%q 
+  
+    # Calculate the hash value of pattern and first window 
+    # of text 
+    for i in xrange(M): 
+        p = (d*p + ord(pat[i]))%q 
+        t = (d*t + ord(txt[i]))%q 
+  
+    # Slide the pattern over text one by one 
+    for i in xrange(N-M+1): 
+        # Check the hash values of current window of text and 
+        # pattern if the hash values match then only check 
+        # for characters on by one 
+        if p==t: 
+            # Check for characters one by one 
+            for j in xrange(M): 
+                if txt[i+j] != pat[j]: 
                     break
-            if match:
-                result = result + [s]
-        # Calculate hash value for next window of text: Remove
-        # leading digit, add trailing digit
-        if s < n - m:
-            t = (t - h * ord(text[s])) % q  # remove letter s
-            t = (t * d + ord(text[s+m])) % q  # add letter s+m
-            t = (t + q) % q  # make sure that t >= 0
-    return "Found pattern at " + str(result) + "."
-
-
-print(rabin_karp_algorithm("Hello World", "ll", 256, 11))
-print(rabin_karp_algorithm("12121212", "12", 256, 11))
-
+  
+            j+=1
+            # if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1] 
+            if j==M: 
+                print "Pattern found at index " + str(i) 
+  
+        # Calculate hash value for next window of text: Remove 
+        # leading digit, add trailing digit 
+        if i < N-M: 
+            t = (d*(t-ord(txt[i])*h) + ord(txt[i+M]))%q 
+  
+            # We might get negative values of t, converting it to 
+            # positive 
+            if t < 0: 
+                t = t+q 
+  
+# Driver program to test the above function 
+txt = "Open Source Contribution"
+pat = "Open Source"
+q = 101 # A prime number 
+search(pat,txt,q)  
