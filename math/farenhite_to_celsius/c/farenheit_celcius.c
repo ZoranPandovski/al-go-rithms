f4609c9ffa991ece1554c67f0ecd8cc5b4d028a4
Sahana Muralikrishnan <sahana@Shubhankar.org>
2018-10-21 20:14:28 +0530

    Adding Test Cases

--- math/farenhite_to_celsius/c/farenheit_celcius.c
+++ math/farenhite_to_celsius/c/farenheit_celcius.c
@@ -3,6 +3,10 @@
  * @Email: sahana_m_student@headstartschool.org
  *
  * C code to convert temperature from farenheit to celcius using formula (F-32)*(5/9)
+ * 
+ * Test with 95 -> 35.00
+ * Test with -12 -> -24.44
+ * Test with 0 -> -17.78
  */
 
 #include<stdio.h>
