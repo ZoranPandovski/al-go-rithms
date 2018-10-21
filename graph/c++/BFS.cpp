fb01c63a4d78ea63e52df36e9049047e66bdc8a0
Rohith <devarshettyrohith9199@gmail.com>
2018-10-21 13:32:08 +0530

    Using "Classes and comments"

--- graph/c++/BFS.cpp
+++ graph/c++/BFS.cpp
@@ -1,5 +1,61 @@
 #include<bits/stdc++.h>
-
+using namespace std;
+class graph
+{
+int V; 
+list<int> *adj; 
+public:
+	graph (int v);
+	void addedge(int v,int m);
+	void bfs(int s);
+};
+graph ::graph(int v)
+{
+	V=v;
+	adj=new list<int>[V];
+}
+void graph:: addedge(int v,int m)
+{
+	adj[v].push_back(m);
+}
+void graph::bfs(int s)
+{
+	bool *visited= new bool[V];
+	for(int i=0;i<V;i++)
+	visited[i]=false;
+	list<int> queue;
+	visited[s]=true;
+	queue.push_back(s);
+	list<int> ::iterator i;
+	while(!queue.empty())
+	{
+		s=queue.front();
+		cout<<s<<"->";
+		queue.pop_front();
+		for(i=adj[s].begin();i!=adj[s].end();++i)
+		{	
+			if(!visited[*i])
+			{
+				visited[*i]=true;
+				queue.push_back(*i);
+				
+			}
+		}	
+	}
+}
+int main()
+{
+	graph g(4);
+	g.addedge(0, 1);
+g.addedge(0, 2);
+g.addedge(1, 2);
+g.addedge(2, 0);
+g.addedge(2, 3);
+g.addedge(3, 3);
+	g.bfs(0);
+	return 0;
+}
+	
 using namespace std;
 
 int vertices;
