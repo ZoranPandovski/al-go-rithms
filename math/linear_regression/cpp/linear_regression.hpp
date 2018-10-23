91e9813d4d4582392ce751e6183ccc09ac3287b5
Zuku271 <adrian.zuchowski@outlook.com>
2018-10-23 22:24:26 +0200

    Minor changes. Function is now a template.

--- math/linear_regression/cpp/linear_regression.hpp
+++ math/linear_regression/cpp/linear_regression.hpp
@@ -4,6 +4,40 @@
 #include <utility>
 #include <vector>
 
-std::pair<double, double> linear_regression(const std::vector<double> &x, const std::vector<double> &y, unsigned int n);
+template <typename T>
+std::pair<T, T> linear_regression(const std::vector<T> &x, const std::vector<T> &y, unsigned int n)
+{
+    T sumOfMul = 0;
+    T sumOfX = 0;
+    T sumOfY = 0;
+    T sumOfSquareX = 0;
+
+    unsigned int i = 0;
+    for (; i < n; ++i)
+    {
+        sumOfMul += x[i] * y[i];
+    }
+    
+    for (i = 0; i < n; ++i)
+    {
+        sumOfX += x[i];
+    }
+
+    for (i = 0; i < n; ++i)
+    {
+        sumOfY += y[i];
+    }
+
+    for (i = 0; i < n; ++i)
+    {
+        sumOfSquareX += x[i] * x[i];
+    }
+    const double denominator = (n*sumOfSquareX - sumOfX*sumOfX);
+
+    const double m = (n*sumOfMul - sumOfX*sumOfY) / denominator;
+    const double b = (sumOfSquareX*sumOfY - sumOfX*sumOfMul) / denominator;
+
+    return std::make_pair(m, b);
+}
 
 #endif
--- math/linear_regression/cpp/main.cpp
+++ math/linear_regression/cpp/main.cpp
@@ -4,13 +4,27 @@
 #include "linear_regression.hpp"
 
 
+/*
+    Linear regression
+
+        n * sum(Xi, Yi) - sum(Xi)*sum(Yi)
+    m = ---------------------------------
+        n * sum(Xi * Xi) - [sum(Xi)]^2
+
+        sum(Xi * Xi) * sum(Yi) - sum(Xi) * sum(Xi, Yi)
+    b = ---------------------------------;
+        n * sum(Xi * Xi) - [sum(Xi)]^2
+
+    It takes 3 arguments: set of (x, y) points and an 'n' as a size of vectors
+    Returns: std::pair(double, double)
+*/
 
 int main()
 {
     std::vector<double> x = {0, 1, 2, 3, 4, 5};
-    std::vector<double> y = {0, 2, 4, 6, 8, 10};
+    std::vector<double> y = {-1, 1, 3, 5, 7, 9};
 
-    std::pair<double, double> res = linear_regression(x, y, 6);
+    std::pair<double, double> res = linear_regression<double>(x, y, 6);
 
     std::cout << res.first << ' ' << res.second << '\n';
 
