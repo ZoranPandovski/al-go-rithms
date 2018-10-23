8d7cfb85f3fce39fe563645801f1803d42f71fda
AhmedRaza <raza15727@gmail.com>
2018-10-23 14:11:56 +0500

    add-raz

--- graph/Java/BFS.java
+++ graph/Java/BFS.java
@@ -63,19 +63,19 @@ class Graph
 	// Driver method to
 	public static void main(String args[])
 	{
-		Graph g = new Graph(4);
+		Graph e = new Graph(4);
 
-		g.addEdge(0, 1);
-		g.addEdge(0, 2);
-		g.addEdge(1, 2);
-		g.addEdge(2, 0);
-		g.addEdge(2, 3);
-		g.addEdge(3, 3);
+		e.addEdge(0, 1);
+		e.addEdge(0, 2);
+		e.addEdge(1, 2);
+		e.addEdge(2, 0);
+		e.addEdge(2, 3);
+		e.addEdge(3, 3);
 
 		System.out.println("Following is Breadth First Traversal "+
 						"(starting from vertex 2)");
 
-		g.BFS(2);
+		e.BFS(2);
 	}
 }
 
