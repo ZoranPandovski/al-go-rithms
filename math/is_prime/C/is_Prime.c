f394c253cddd8c3da837fb02819caaaf86058a1e
Lucas Eduardo Schlogl <leschlogl@gmail.com>
2018-10-24 09:47:19 -0300

    Correcting the C algorithm
    The C implementation of is prime was wrong. I've changed the method is_prime to work fine.

--- math/is_prime/C/is_Prime.c
+++ math/is_prime/C/is_Prime.c
@@ -3,18 +3,18 @@
 
 // Function that returns 1 if n is Prime or 0 is it is not prime
 int isPrime(int n){
-	int i=2;
-	while(i*i<=n){
-		if (n%i==0) return 1;
+	int i = 2;
+	while(i < n){
+		if (n % i == 0) return 0;
 		i++;
 	}
-	return 0;
+	return 1;
 }
 
 int main()
 {
 	int i;
-	for(i=1;i<11;i++){
+	for(i=1;i<30;i++){
 		if(isPrime(i)) 
 			printf("%d is Prime\n",i);
 	}
