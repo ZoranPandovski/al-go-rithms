c66b43868dd3c09e8669406a2d9659e5087638f2
Przemek Bajkowski <pb.pb@onet.pl>
2018-10-25 20:12:29 +0200

    Greatest digit in number

---
+++ math/basic/Greatest_digit_in_number/javascript/greatest_digit_in_number.js
@@ -0,0 +1,16 @@
+function greatest_digit_in_number(num) {
+	num = num.toString();
+	var a = num.split("");
+	var max = a[0];
+	a.forEach(element => {
+		if(max < element) {
+			max = element;
+		}
+	});
+	return max;
+}
+
+// Example
+var number = 37847862;
+var digit = greatest_digit_in_number(number);
+console.log(digit); // Expected output: 8
