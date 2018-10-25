d9ddd11962e053142e7c61a1df29925e3d6300c3
Tobias <germanicalp@gmail.com>
2018-10-25 10:30:03 +0200

    Added Fahrenheit to Celsius converter in Python and fixed typo in directory name

---
+++ math/farenheit_to_celsius/python/fahrenheit_to_celsius.py
@@ -0,0 +1,5 @@
+fahrenheit = int(input("Enter a temperature in Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5.0/9.0
+
+print("The Temperature of", fahrenheit, "°F equals", celsius, "°C")
--- math/farenhite_to_celsius/js/farenhite_to_celsius.js
+++
@@ -1,7 +0,0 @@
-const fahrenheit = prompt('Enter Fahrenheit value:');
-
-const fahrenheitToCelsius = (number) => {
-    return ((number - 32) * 5/9).toFixed(1);
-}
-
-document.write(`${fahrenheit} Fahrenheit is ${fahrenheitToCelsius(fahrenheit)} Celsius.`);
