72ced3827142454135af63ce6facabcb9b968c1f
shreyasingh28 <44510510+shreyasingh28@users.noreply.github.com>
2018-10-27 02:01:12 +0530

    updated elixir code

---
+++ sort/bubble_sort/bubble.ex
@@ -0,0 +1,25 @@
+defmodule Selection do
+  def sort(list) when is_list(list) do
+    do_selection(list, [])
+  end
+
+  def do_selection([head|[]], acc) do
+    acc ++ [head]
+  end
+  def do_selection(list, acc) do
+    min = min(list)
+    do_selection(:lists.delete(min, list), acc ++ [min])
+  end
+
+  defp min([first|[second|[]]]) do
+    smaller(first, second)
+  end
+  defp min([first|[second|tail]]) do
+    min([smaller(first, second)|tail])
+  end
+
+  defp smaller(e1, e2) do
+    if e1 <= e2 do e1 else e2 end
+  end
+end
+IO.inspect Selection.sort([100,4,10,6,9,3]) #=> [1, 3, 4, 6, 9, 10]
