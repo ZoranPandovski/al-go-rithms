411bea529daacb7b6fcbcfa5c03052c8462a775e
HigorSnt <higorsantos600@gmail.com>
2018-10-25 22:56:00 -0300

    decimal_to_bin_oct_hex in MATLAB

---
+++ math/decimal_to_bin_oct_hex/matlab/decimal_to_bin_oct_hex.m
@@ -0,0 +1,4 @@
+x = input('Enter the amount to be converted: ');
+fprintf('%d in binary is: %s\n', x, dec2bin(x));
+fprintf('%d in octal is: %s\n', x, dec2base(x, 8));
+fprintf('%d in hexadecimal is: %s\n', x, dec2hex(x));
