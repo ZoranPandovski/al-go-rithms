36bb75ed2446c149dc25f0e31e17de686c714ae0
Arjun Shibu <43996156+dar0ng1@users.noreply.github.com>
2018-10-26 19:51:54 +0530

    Added fibonacci_sequence.py
    Prints fibonacci sequence upto n terms using function recursion method

---
+++ math/fibonacci/python/fibonacci_sequence.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter number of terms:"))
+print("Fibonacci sequence:")
+for i in range(n):
+    print fibonacci(i),
