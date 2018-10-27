80ff8b7c1e7b2bfce57da5d0bc8686e49ba43774
ROHIT KUMAR <rkrohitkmr93@gmail.com>
2018-10-27 18:12:28 +0530

    Update identical_trees.py

--- data_structures/Tree/python/identical_trees.py
+++ data_structures/Tree/python/identical_trees.py
@@ -6,7 +6,7 @@ from tree_utils import TreeNode
 def identical_trees(root1, root2):
 	if not root1 and not root2:
 		return True
-	if root1 and root2:
+	elif root1 and root2:
 		return (root1.data == root2.data) and \
 				identical_trees(root1.left, root2.left) and \
 				identical_trees(root1.right, root2.right)
