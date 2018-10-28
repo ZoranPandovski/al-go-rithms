72bcf8add42353f892610f7d665fde1f1afde483
melvin.abraham1996@gmail.com <melvin.abraham1996@gmail.com>
2018-10-28 16:54:45 -0700

    Merging two sorted linked lists

---
+++ Linked_list/C++/merget_two_sorted_linked_lists.cpp
@@ -0,0 +1,27 @@
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+  	ListNode *prev = NULL;
+  	ListNode *cur = head;
+  	ListNode *successor = cur;
+
+  	while(cur != NULL)	{
+
+  		successor = successor->next;
+  		cur->next = prev;
+  		prev = cur;
+  		cur = successor;
+
+  	}
+
+  	return prev;
+    }
+};
