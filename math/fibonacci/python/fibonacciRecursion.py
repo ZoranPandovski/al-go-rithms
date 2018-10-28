8007c19163dc03274d4bf1a1b937f0a362ee9ad4
seemantaggarwal <32275622+seemantaggarwal@users.noreply.github.com>
2018-10-29 02:49:34 +0530

    fibonacci via recursion python

---
+++ math/fibonacci/python/fibonacciRecursion.py
@@ -0,0 +1,20 @@
+def recur_fibo(n):
+ 
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+# Change this value for a different result
+nterms = 10
+
+# uncomment to take input from the user
+#nterms = int(input("How many terms? "))
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
