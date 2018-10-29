81aa2024093e3fe0f20823c0ee60c1b90d5893ec
zilehuda <zilehuda.tariq@gmail.com>
2018-10-29 22:21:42 +0500

    added linkedlist

---
+++ linkedlist_zilehuda/cpp/linkedlist.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+
+
+class Node
+{
+public:
+    Node* next;
+    int data;
+};
+
+using namespace std;
+
+class LinkedList
+{
+public:
+    int length;
+    Node* head;
+
+    LinkedList();
+    ~LinkedList();
+    void add(int data);
+    void print();
+};
+
+LinkedList::LinkedList(){
+    this->length = 0;
+    this->head = NULL;
+}
+
+LinkedList::~LinkedList(){
+    std::cout << "LIST DELETED";
+}
+
+void LinkedList::add(int data){
+    Node* node = new Node();
+    node->data = data;
+    node->next = this->head;
+    this->head = node;
+    this->length++;
+}
+
+void LinkedList::print(){
+    Node* head = this->head;
+    int i = 1;
+    while(head){
+        std::cout << i << ": " << head->data << std::endl;
+        head = head->next;
+        i++;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    LinkedList* list = new LinkedList();
+    for (int i = 0; i < 100; ++i)
+    {
+        list->add(rand() % 100);
+    }
+    list->print();
+    std::cout << "List Length: " << list->length << std::endl;
+    delete list;
+    return 0;
+}
