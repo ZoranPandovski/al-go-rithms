b81c4eb1155ebf5f620d5758ad70fc0163293031
Aakash1822 <30810386+Aakash1822@users.noreply.github.com>
2018-10-30 02:23:00 +0530

    Update binary_search,cpp
    A recursive way of doing this mathematics problem

--- data_structures/Arrays/CPP/binary_search_array .cpp
+++ data_structures/Arrays/CPP/binary_search_array .cpp
@@ -1,47 +1,40 @@
-#include<iostream>
-using namespace std;
-void search(int x[], int p, int q, int r)
-{
-	int mid=(p+q)/2;
-	if(x[mid]==r)
-	{
-		cout<<"Found at posiiton "<<mid+1;
-		return;
-	}
-	else if(r>x[mid])
-	search(x,mid+1,q,r);
-	else if(r<x[mid])
-	search(x,p,mid-1,r);
-	else
-	{
-		cout<<"Element not found";
-		return;
-	}
-}
+## using Recursion Binary search implementation #################
 
-int main(){
-    int arr[100],i,j,k,l,n,temp,flag=0;
-    cout<<"Enter the size of the array: "<<endl;
-    cin>>n;
-    cout<<"Enter the elements"<<endl;
-    for(i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(arr[j]<arr[i]){
-                temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
-    cout<<"Sorted array is"<<endl;
-    for(i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl<<"enter the element you want to search"<<endl;
-    cin>>l;
-    search(arr,0,n-1,l);
-	return 0;
-}
+int binarySearch(int arr[], int right, int left, int x){
+	
+	while(left<=right){
+		
+		int mid = left + (right-left)/2;
+			
+			if(arr[mid]==x){
+				
+				return m;
+			}
+          if(arr[mid] <x){
+		  
+		  left = md+1;
+	  }
+		else 
+		{ right = mid-1;
+		
+		return -1;
+	}
+		
+		int main(void){
+			
+			int arr[5] = {2, 4,7,9,8,5};
+			
+			int n = sizeof(arr)/ sizeof(arr[0]);
+			int x=9;
+			
+			int outcome = binarySearch(arr,0,n-1, x);
+                         
+			(result == -1)?
+				cout<< outcome
+				
+				return 0;
+			
+			
+			
+			
+			
