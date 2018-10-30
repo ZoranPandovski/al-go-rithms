6e6b9c46bfb84430dfe1dab9fedb8a7301e76dc1
hafpaf <haf@hafnium.me>
2018-10-30 20:52:49 +0100

    add user input

--- math/sum_of_numbers/sum_of_numbers.py
+++ math/sum_of_numbers/sum_of_numbers.py
@@ -3,7 +3,8 @@ def sum_of_numbers_iterative(n):
     for i in range(n):
         the_sum += (i+1)
     return the_sum
-print(sum_of_numbers_iterative(5))
+print(sum_of_numbers_iterative(int(input("Enter a number: "))))
+#5+4+3+2+1=15
 
 #Too high a number give:
 #RecursionError: maximum recursion depth exceeded in comparison
