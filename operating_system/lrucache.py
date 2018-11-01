080803986b46f9c8b3548f8c62e73ac4573a78b7
agrimabahl <agrimabahl@users.noreply.github.com>
2018-10-31 23:08:37 -0700

    added LRU cache to OS algorithms

---
+++ operating_system/lrucache.py
@@ -0,0 +1,23 @@
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tm = 0
+        self.cache = {}
+        self.lru = {}
+
+    def get(self, key):
+        if key in self.cache:
+            self.lru[key] = self.tm
+            self.tm += 1
+            return self.cache[key]
+        return -1
+
+    def set(self, key, value):
+        if len(self.cache) >= self.capacity:
+            # find the LRU entry
+            old_key = min(self.lru.keys(), key=lambda k:self.lru[k])
+            self.cache.pop(old_key)
+            self.lru.pop(old_key)
+        self.cache[key] = value
+        self.lru[key] = self.tm
+        self.tm += 1
