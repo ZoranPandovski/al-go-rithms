c2de002cb6419c8b2c4eb64429964e228c9332d6
Ghulam Mohiyuddin <subahani98@gmail.com>
2019-10-21 23:25:53 +0530

    Add files
    count set bits

--- bit_manipulation/count_set_bits/Python/count_set_bits.py
+++ bit_manipulation/count_set_bits/Python/count_set_bits.py
@@ -6,8 +6,8 @@ Explaination:
 Binary representation of of 7 is "111" and number of set bits(ones) is 3.
 
 
-Input 1:23
-Output 1:4
+Input 2:23
+Output 2:4
 
 Explain:
 Binary representation of of 7 is "10111" and number of set bits(ones) is 4. 
