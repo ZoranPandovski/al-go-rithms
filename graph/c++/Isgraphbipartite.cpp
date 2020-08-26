92b364b8e48822acddb1a4ffee19107d55b7fcf8
Saloni Sawarkar <61061499+Salonii02@users.noreply.github.com>
2020-08-27 02:19:27 +0530

    Update Isgraphbipartite.cpp
    Adding test cases for reference.

--- graph/c++/Isgraphbipartite.cpp
+++ graph/c++/Isgraphbipartite.cpp
@@ -84,3 +84,12 @@ public:
     }
         
 };
+//TESTs
+//Sample
+/*Input: [[1,3], [0,2], [1,3], [0,2]]
+Output: true
+Input: [[1,2,3], [0,2], [0,1,3], [0,2]]
+Output: false
+....for disconnected graph
+Input:[[],[2,4,6],[1,4,8,9],[7,8],[1,2,8,9],[6,9],[1,5,7,8,9],[3,6,9],[2,3,4,6,9],[2,4,5,6,7,8]]
+Output:true
