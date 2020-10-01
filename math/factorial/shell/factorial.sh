241ebd6252166f51e9a0afea968290cf88c9f318
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2020-10-01 00:54:41 -0300

    factorial in shell

---
+++ math/factorial/shell/factorial.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+factorial()
+{
+  if [ "$1" -gt "1" ]; then
+    i=`expr $1 - 1`
+    j=`factorial $i`
+    k=`expr $1 \* $j`
+    echo $k
+  else
+    echo 1
+  fi
+}
+
+echo "Enter a number:"
+read x
+factorial $x
