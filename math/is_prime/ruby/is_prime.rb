8e904376bbabfd2716d9dc6954257a2afee63032
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2020-10-01 18:57:53 -0300

    refactoring is_prime

--- math/is_prime/ruby/is_prime.rb
+++ math/is_prime/ruby/is_prime.rb
@@ -1,18 +1,18 @@
 puts "Enter a number: "
 number = gets.to_i
 
-aux = number-1
+aux = number-1 	# numbers between "number" and "one", for test your divisibility
 is_divisible = 0
 
 while aux > 1 do
-  if number%aux == 0
-    is_divisible = 1
+  if number%aux == 0	# zero indicates with number is divisible for other, than himself or one
+    is_divisible = 1	
     break
   end
   aux -= 1
 end
 
-if  is_divisible == 0 or number == 2
+if  is_divisible == 0
   puts "The number " + number.to_s + " is prime\n"
 else
   puts "The number " + number.to_s + " is not prime\n"
