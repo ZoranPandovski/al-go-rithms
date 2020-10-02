67e2d71a46bab77e25113eaa033a5ef0199c526f
TeeVenDick <teevendick@gmail.com>
2020-10-02 08:44:34 +0800

    Added caesar cipher algorithm in Matlab

---
+++ cryptography/caesar_cipher/Matlab/caesar_cipher.m
@@ -0,0 +1,62 @@
+decision = input('Encryption or decryption?\n\n1 = Encryption\n2 = Decryption\n\nChoice: ');
+text = input('\nPlease input plain text: ', 's');
+key = input('Shift amount: ');
+loop = length(text);
+
+if decision == 1
+    a = encrypt(loop, key, text);
+elseif decision == 2
+    a = decrypt(loop, key, text);
+else 
+    disp('Please input the correct choices')
+end  
+
+function  a = encrypt(loop, key, text)                 
+    for no = 1:loop                                    
+        current = text(no);
+        
+        if(current >= 'a' && current <= 'z')           
+            current = current + key;                   
+            
+            if(current > 'z')                           
+                current = current - 'z' + 'a' -1;
+            end
+                                                    
+        elseif(current >= 'A' && current <= 'Z')
+            current = current + key;
+            
+            if(current > 'Z')
+                current = current - 'Z' + 'A' -1;      
+            end
+        end
+        
+        text(no) = current;
+    end
+    a = text;
+    fprintf('Ciphered text: %s \n', a)
+end
+
+function  a = decrypt(loop, key, text)
+    for no = 1:loop
+        current = text(no);
+        
+        if(current >= 'a' && current <= 'z')       
+            current = current - key;                  
+            
+            if(current < 'a')
+                current = current + 'z' - 'a' +1;     
+            end                                     
+                                                  
+        elseif(current >= 'A' && current <= 'Z')      
+            current = current - key;
+            
+            if(current < 'A')
+                current = current + 'Z' - 'A' +1;
+            end
+        end
+        
+        text(no) = current;
+    end
+    a = text;
+    fprintf('Plain text: %s \n', a)
+end
