bb119f83753f17bbdf3e4cd5815a212966e3ddfe
Shubham Jha <shubham.jha2001m@gmail.com>
2020-10-03 11:32:00 +0700

    Palindrome in C++

---
+++ strings/palindrome/cpp/palindrome.cpp
@@ -0,0 +1,29 @@
+// C++ Program To Check Whether String is Palindrome or Not 
+// Without using STL.
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    char str1[20], str2[20];
+    
+    int i, j, len = 0, flag = 0;
+    
+    cout << "Enter A String : ";
+    gets(str1);
+    
+    len = strlen(str1) - 1;
+    
+    for (i = len,j = 0; i>=0; i--, j++)
+        str2[j] = str1[i];
+        
+    if (strcmp(str1, str2))
+        cout << str1 << " is Not a Palindrome";
+    else
+        cout << str1 << " is a Palindrome";
+        
+    return 0;
+}
