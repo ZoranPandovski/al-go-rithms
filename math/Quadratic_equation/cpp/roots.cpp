96abba9d55687e199ad91508c696396e8c68e7de
Krishna Kumar <57854052+Krishna3102@users.noreply.github.com>
2020-10-06 06:22:22 +0530

    Update roots.cpp

--- math/Quadratic_equation/cpp/roots.cpp
+++ math/Quadratic_equation/cpp/roots.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
+#include <math.h>
 #include <cmath>
 using namespace std;
-
-int main() {
-
-    float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
-    cout << "Enter coefficients a, b and c: ";
+int main ()
+{
+    float a, b, c;
     cin >> a >> b >> c;
-    discriminant = b*b - 4*a*c;
-
-    if (discriminant > 0) {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-        x2 = (-b - sqrt(discriminant)) / (2*a);
-        cout << "Roots are real and different." << endl;
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
+    float d;
+    float d1;
+    d = b*b - 4*a*c;
+    d1 = sqrt(d);
+    if (d == 0)
+        cout << "Real and Equal";
+    else if (d >= 0)
+        cout << "Real and Distinct";
+    else if (d < 0)
+        cout << "Imaginary";
+    cout << endl;
+    int x1, x2;
+    if (d >= 0)
+    {  
+    x1 = (-b - d1)/(2*a);
+    x2 = (-b + d1)/(2*a);
+    cout << x1 << " " << x2;
     }
-
-    else if (discriminant == 0) {
-        cout << "Roots are real and same." << endl;
-        x1 = (-b) / (2*a);
-        cout << "x1 = x2 =" << x1 << endl;
-    }
-
-    else {
-        realPart = -b/(2*a);
-        imaginaryPart =sqrt(-discriminant)/(2*a);
-        cout << "Roots are complex and different."  << endl;
-        cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
-        cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
-    }
-
     return 0;
 }
+
