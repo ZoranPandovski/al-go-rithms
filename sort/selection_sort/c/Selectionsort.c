befc08effb9540092861ed05a0af3a788c88c6b7
Divakar Lakhera <divakarlakhera99@gmail.com>
2020-10-16 12:18:46 +0530

    Revert "add Kadane Algorithm (python)"
    This reverts commit f3dff299d6b27821235cd2c5f2b6942f1b90a949.

--- sort/selection_sort/c/Selectionsort.c
+++ sort/selection_sort/c/Selectionsort.c
@@ -1,29 +1,67 @@
-#include <stdio.h>
+//selection sorting in C Programming
+//This is beginner friendly selection sorting as it has just a simple for loop used
+#include<stdio.h>
+#include<stdlib.h>
 
-void change_position(int *positionI, int *positionJ){
-	int aux;
+int i,n;
 
-	aux = *positionI;
-	*positionI = *positionJ;
-	*positionJ = aux;
+void input();//function prototype
+void display(int b[20]);//function prototype
+void sorting(int c[20]);//function prototype
+
+int main(){
+    input();
+    return 0;
 }
-	
-void selection_sort(int *vector, int Max_size){
-	int i, j, position;
 
-	for(i=0;i<(Max_size - 1);i++){
-		position = i;
-		
-		for(j = i+1;j <Max_size;j++){
-			
-			if(vector[j] < vector[position]){
-				position = j;
-			}
-		}
-
-		if(i != position){
-			change_position(&vector[i],&vector[position]);
-		}
-	}
-
+void input(){
+    int a[20];
+    printf("Enter the total numbers you want to enter\t");
+    scanf("%d",&n);
+    printf("Enter the numbers you want to enter\t");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("The entered numbers are:\n");
+    display(a);
+    sorting(a);
 }
+
+void display(int b[20]){
+    for(i=0;i<n;i++){
+        printf("%d\t",b[i]);
+    }
+}
+
+void sorting(int c[20]){
+    int j,temp;
+    //selection sorting algorithm for ascending order
+    for(i=0;i<(n-1);i++){
+        for(j=i+1;j<n;j++){
+            if(c[i]>c[j]){
+                temp=c[i];
+                c[i]=c[j];
+                c[j]=temp;
+            }//if-close
+        }//j loop
+    }//i loop
+    printf("\nThe result after sorting in ascending order is:\n");
+    display(c);
+
+    //selection sorting algorithm for descending order
+    for(i=0;i<(n-1);i++){
+        for(j=i+1;j<n;j++){
+            if(c[i]<c[j]){
+                temp=c[i];
+                c[i]=c[j];
+                c[j]=temp;
+            }//if-close
+        }//j loop
+    }//i loop
+    printf("\nThe result after sorting in descending order is:\n");
+    display(c);
+}
+
+
+
+
