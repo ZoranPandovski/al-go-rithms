ce16fd601088709d100bd6a1e74a1b8c4e63a0cb
Muthusamy K <31694928+KMuthusamyms@users.noreply.github.com>
2020-10-19 15:46:27 +0530

    Create bubble_javascript.js
    Bubble sort in JavaScript

---
+++ bubble_sort/javascript/bubble_javascript.js
@@ -0,0 +1,22 @@
+function swap(arr, first_Index, second_Index){
+    var temp = arr[first_Index];
+    arr[first_Index] = arr[second_Index];
+    arr[second_Index] = temp;
+}
+
+function bubble_Sort(arr){
+
+    var len = arr.length,
+        i, j, stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (arr[j] > arr[j+1]){
+                swap(arr, j, j+1);
+            }
+        }
+    }
+
+    return arr;
+}
+console.log(bubble_Sort([3, 0, 2, 5, -1, 4, 1]));
