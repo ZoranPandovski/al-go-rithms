c1cee112609bedd62fe0d02db747409917afd1ce
Christian Clauss <cclauss@me.com>
2020-10-26 09:54:42 +0100

    Fix IndentationError in array_equilib.py
    Pull requests should not be merged if they have failing tests.

--- data_structures/Arrays/Python/array_equilib.py
+++ data_structures/Arrays/Python/array_equilib.py
@@ -1,12 +1,10 @@
 def equilibriumIndex(arr):
     # Please add your code here
-    ind = arr.index(sum(arr))
-    return ind if ind else -1
-    pass
+    return arr.index(sum(arr)) or -1
 
-# Main
- n = int(input())
- arr = [int(i) for i in input().strip().split()]
-#n=7
-#arr = [-7, 1, 5, 2, -4, 3, 0]
-print(equilibriumIndex(arr))
+if __name__ == "__main__":
+    n = int(input())
+    arr = [int(i) for i in input().strip().split()]
+    #n=7
+    #arr = [-7, 1, 5, 2, -4, 3, 0]
+    print(equilibriumIndex(arr))
