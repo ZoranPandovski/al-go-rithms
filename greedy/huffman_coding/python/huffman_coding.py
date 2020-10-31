fd2b6a8b8a19dfe472c4ddfd30bf89b29ae882a2
AYUSH JAIN <55896515+codiyapaa@users.noreply.github.com>
2020-10-31 20:50:16 +0530

    updated huffman_coding.py
    Successfully working and updated

--- greedy/huffman_coding/python/huffman_coding.py
+++ greedy/huffman_coding/python/huffman_coding.py
@@ -1,182 +1,104 @@
-import heapq
-import os
-from functools import total_ordering
+import math
+import sys
+global probabilities
+probabilities = []
 
-"""
-Code for Huffman Coding, compression and decompression. 
-Explanation at http://bhrigu.me/blog/2017/01/17/huffman-coding-python-implementation/
-"""
+class HuffmanCode:
+    def __init__(self,probability):
+        self.probability = probability
 
-@total_ordering
-class HeapNode:
-	def __init__(self, char, freq):
-		self.char = char
-		self.freq = freq
-		self.left = None
-		self.right = None
+    def position(self, value, index):
+        for j in range(len(self.probability)):
+            if(value >= self.probability[j]):
+                return j
+        return index-1
 
-	# defining comparators less_than and equals
-	def __lt__(self, other):
-		return self.freq < other.freq
+    def characteristics_huffman_code(self, code):
+        length_of_code = [len(k) for k in code]
 
-	def __eq__(self, other):
-		if(other == None):
-			return False
-		if(not isinstance(other, HeapNode)):
-			return False
-		return self.freq == other.freq
+        mean_length = sum([a*b for a, b in zip(length_of_code, self.probability)])
 
+        print("Average length of the code: %f" % mean_length)
+        print("Efficiency of the code: %f" % (entropy_of_code/mean_length))
 
-class HuffmanCoding:
-	def __init__(self, path):
-		self.path = path
-		self.heap = []
-		self.codes = {}
-		self.reverse_mapping = {}
+    def compute_code(self):
+        num = len(self.probability)
+        huffman_code = ['']*num
 
-	# functions for compression:
+        for i in range(num-2):
+            val = self.probability[num-i-1] + self.probability[num-i-2]
+            if(huffman_code[num-i-1] != '' and huffman_code[num-i-2] != ''):
+                huffman_code[-1] = ['1' + symbol for symbol in huffman_code[-1]]
+                huffman_code[-2] = ['0' + symbol for symbol in huffman_code[-2]]
+            elif(huffman_code[num-i-1] != ''):
+                huffman_code[num-i-2] = '0'
+                huffman_code[-1] = ['1' + symbol for symbol in huffman_code[-1]]
+            elif(huffman_code[num-i-2] != ''):
+                huffman_code[num-i-1] = '1'
+                huffman_code[-2] = ['0' + symbol for symbol in huffman_code[-2]]
+            else:
+                huffman_code[num-i-1] = '1'
+                huffman_code[num-i-2] = '0'
 
-	def make_frequency_dict(self, text):
-		frequency = {}
-		for character in text:
-			if not character in frequency:
-				frequency[character] = 0
-			frequency[character] += 1
-		return frequency
+            position = self.position(val, i)
+            probability = self.probability[0:(len(self.probability) - 2)]
+            probability.insert(position, val)
+            if(isinstance(huffman_code[num-i-2], list) and isinstance(huffman_code[num-i-1], list)):
+                complete_code = huffman_code[num-i-1] + huffman_code[num-i-2]
+            elif(isinstance(huffman_code[num-i-2], list)):
+                complete_code = huffman_code[num-i-2] + [huffman_code[num-i-1]]
+            elif(isinstance(huffman_code[num-i-1], list)):
+                complete_code = huffman_code[num-i-1] + [huffman_code[num-i-2]]
+            else:
+                complete_code = [huffman_code[num-i-2], huffman_code[num-i-1]]
 
-	def make_heap(self, frequency):
-		for key in frequency:
-			node = HeapNode(key, frequency[key])
-			heapq.heappush(self.heap, node)
+            huffman_code = huffman_code[0:(len(huffman_code)-2)]
+            huffman_code.insert(position, complete_code)
 
-	def merge_nodes(self):
-		while(len(self.heap)>1):
-			node1 = heapq.heappop(self.heap)
-			node2 = heapq.heappop(self.heap)
+        huffman_code[0] = ['0' + symbol for symbol in huffman_code[0]]
+        huffman_code[1] = ['1' + symbol for symbol in huffman_code[1]]
 
-			merged = HeapNode(None, node1.freq + node2.freq)
-			merged.left = node1
-			merged.right = node2
+        if(len(huffman_code[1]) == 0):
+            huffman_code[1] = '1'
 
-			heapq.heappush(self.heap, merged)
+        count = 0
+        final_code = ['']*num
 
+        for i in range(2):
+            for j in range(len(huffman_code[i])):
+                final_code[count] = huffman_code[i][j]
+                count += 1
 
-	def make_codes_helper(self, root, current_code):
-		if(root == None):
-			return
+        final_code = sorted(final_code, key=len)
+        return final_code
 
-		if(root.char != None):
-			self.codes[root.char] = current_code
-			self.reverse_mapping[current_code] = root.char
-			return
+string = input("Enter the string to compute Huffman Code: ")
 
-		self.make_codes_helper(root.left, current_code + "0")
-		self.make_codes_helper(root.right, current_code + "1")
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
 
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+length = len(string)
 
-	def make_codes(self):
-		root = heapq.heappop(self.heap)
-		current_code = ""
-		self.make_codes_helper(root, current_code)
+probabilities = [float("{:.2f}".format(frequency[1]/length)) for frequency in freq]
+probabilities = sorted(probabilities, reverse=True)
 
+huffmanClassObject = HuffmanCode(probabilities)
+P = probabilities
 
-	def get_encoded_text(self, text):
-		encoded_text = ""
-		for character in text:
-			encoded_text += self.codes[character]
-		return encoded_text
+huffman_code = huffmanClassObject.compute_code()
 
+print(' Char | Huffman code ')
+print('----------------------')
 
-	def pad_encoded_text(self, encoded_text):
-		extra_padding = 8 - len(encoded_text) % 8
-		for i in range(extra_padding):
-			encoded_text += "0"
+for id,char in enumerate(freq):
+    if huffman_code[id]=='':
+        print(' %-4r |%12s' % (char[0], 1))
+        continue
+    print(' %-4r |%12s' % (char[0], huffman_code[id]))
 
-		padded_info = "{0:08b}".format(extra_padding)
-		encoded_text = padded_info + encoded_text
-		return encoded_text
-
-
-	def get_byte_array(self, padded_encoded_text):
-		if(len(padded_encoded_text) % 8 != 0):
-			print("Encoded text not padded properly")
-			exit(0)
-
-		b = bytearray()
-		for i in range(0, len(padded_encoded_text), 8):
-			byte = padded_encoded_text[i:i+8]
-			b.append(int(byte, 2))
-		return b
-
-
-	def compress(self):
-		filename, file_extension = os.path.splitext(self.path)
-		output_path = filename + ".bin"
-
-		with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
-			text = file.read()
-			text = text.rstrip()
-
-			frequency = self.make_frequency_dict(text)
-			self.make_heap(frequency)
-			self.merge_nodes()
-			self.make_codes()
-
-			encoded_text = self.get_encoded_text(text)
-			padded_encoded_text = self.pad_encoded_text(encoded_text)
-
-			b = self.get_byte_array(padded_encoded_text)
-			output.write(bytes(b))
-
-		print("Compressed")
-		return output_path
-
-
-	""" functions for decompression: """
-
-
-	def remove_padding(self, padded_encoded_text):
-		padded_info = padded_encoded_text[:8]
-		extra_padding = int(padded_info, 2)
-
-		padded_encoded_text = padded_encoded_text[8:] 
-		encoded_text = padded_encoded_text[:-1*extra_padding]
-
-		return encoded_text
-
-	def decode_text(self, encoded_text):
-		current_code = ""
-		decoded_text = ""
-
-		for bit in encoded_text:
-			current_code += bit
-			if(current_code in self.reverse_mapping):
-				character = self.reverse_mapping[current_code]
-				decoded_text += character
-				current_code = ""
-
-		return decoded_text
-
-
-	def decompress(self, input_path):
-		filename, file_extension = os.path.splitext(self.path)
-		output_path = filename + "_decompressed" + ".txt"
-
-		with open(input_path, 'rb') as file, open(output_path, 'w') as output:
-			bit_string = ""
-
-			byte = file.read(1)
-			while(len(byte) > 0):
-				byte = ord(byte)
-				bits = bin(byte)[2:].rjust(8, '0')
-				bit_string += bits
-				byte = file.read(1)
-
-			encoded_text = self.remove_padding(bit_string)
-
-			decompressed_text = self.decode_text(encoded_text)
-			
-			output.write(decompressed_text)
-
-		print("Decompressed")
-		return output_path
+huffmanClassObject.characteristics_huffman_code(huffman_code)
