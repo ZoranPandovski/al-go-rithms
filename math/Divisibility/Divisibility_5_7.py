76d3f8fa319c3269e4203b80b6d8dfe7e08cb129
Sahana <hola.sahana@gmail.com>
2021-05-23 11:24:01 +0530

    Editing divisibility

--- math/Divisibility/Divisibility_5_7.py
+++ math/Divisibility/Divisibility_5_7.py
@@ -1,22 +1,9 @@
 List = []
 
-
-
 boundary = int(input("Enter a boundary value till which you want to carry out the divisibity test : "))
 
 for i in range (1,boundary + 1 ):
-
-
- 
-   
-	if (i%7) == 0 and (i%5) == 0:
-
-
-   
-     
-		List.append(i) 
-
-
-
-
+    if (i%7) == 0 and (i%5) == 0:
+        List.append(i)
 print (List)
+
