76eff0731c9baefefa52e758bd0b98cf1246fa5e
Greg James <gjames4@hawk.iit.edu>
2021-10-04 00:51:19 -0500

    Added Object Pool structure

---
+++ data_structures/Object Pool/C#/ObjectPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace OctaneDownloadEngine
+{
+    public class ObjectPool<T>
+    {
+        private ConcurrentBag<T> _objects;
+        private readonly Func<T> _objectGenerator;
+
+        public ObjectPool(Func<T> objectGenerator)
+        {
+            _objectGenerator = objectGenerator ?? throw new ArgumentNullException(nameof(objectGenerator));
+            _objects = new ConcurrentBag<T>();
+        }
+
+        public T Get() => _objects.TryTake(out T item) ? item : _objectGenerator();
+
+        public void Return(T item) => _objects.Add(item);
+
+        public void Empty()
+        {
+            _objects = null;
+        }
+    }
+}
