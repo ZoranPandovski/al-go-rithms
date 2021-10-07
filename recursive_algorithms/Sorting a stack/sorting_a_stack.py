947c3b52742afeefae6c31426bc4746cbd128df3
rishabh99-rc <>
2021-10-07 19:24:44 +0000

    Updated Sorting_A_Stack.py again

--- recursive_algorithms/Sorting a stack/sorting_a_stack.py
+++ recursive_algorithms/Sorting a stack/sorting_a_stack.py
@@ -1,63 +1,51 @@
-# Python program to sort a stack using recursion
-
-# Recursive method to insert element in sorted way
-
-
-def sortedInsert(s, element):
-
-	# Base case: Either stack is empty or newly inserted
-	# item is greater than top (more than all existing)
-	if len(s) == 0 or element > s[-1]:
-		s.append(element)
-		return
-	else:
-
-		# Remove the top item and recur
-		temp = s.pop()
-		sortedInsert(s, element)
-
-		# Put back the top item removed earlier
-		s.append(temp)
-
-# Method to sort stack
-
-
-def sortStack(s):
-
-	# If stack is not empty
-	if len(s) != 0:
-
-		# Remove the top item
-		temp = s.pop()
-
-		# Sort remaining stack
-		sortStack(s)
-
-		# Push the top item back in sorted stack
-		sortedInsert(s, temp)
-
-# Printing contents of stack
-
-
-def printStack(s):
-	for i in s[::-1]:
-		print(i, end=" ")
-	print()
-
-
-# Driver Code
+from collections import deque
+ 
+ 
+# Insert the given key into the sorted stack while maintaining its sorted order.
+# This is similar to the recursive insertion sort routine
+def sortedInsert(stack, key):
+ 
+    # base case: if the stack is empty or
+    # the key is greater than all elements in the stack
+    if not stack or key > stack[-1]:
+        stack.append(key)
+        return
+ 
+    ''' We reach here when the key is smaller than the top element '''
+ 
+    # remove the top element
+    top = stack.pop()
+ 
+    # recur for the remaining elements in the stack
+    sortedInsert(stack, key)
+ 
+    # insert the popped element back into the stack
+    stack.append(top)
+ 
+ 
+# Recursive method to sort a stack
+def sortStack(stack):
+ 
+    # base case: stack is empty
+    if not stack:
+        return
+ 
+    # remove the top element
+    top = stack.pop()
+ 
+    # recur for the remaining elements in the stack
+    sortStack(stack)
+ 
+    # insert the popped element back into the sorted stack
+    sortedInsert(stack, top)
+ 
+ 
 if __name__ == '__main__':
-	s = []
-	s.append(30)
-	s.append(-5)
-	s.append(18)
-	s.append(14)
-	s.append(-3)
-
-	print("Stack elements before sorting: ")
-	printStack(s)
-
-	sortStack(s)
-
-	print("\nStack elements after sorting: ")
-	printStack(s)
+ 
+    A = [5, -2, 9, -7, 3]
+ 
+    stack = deque(A)
+ 
+    print("Stack before sorting:", list(stack))
+    sortStack(stack)
+    print("Stack after sorting:", list(stack))
