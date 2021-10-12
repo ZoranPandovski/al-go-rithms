04e3a388b31d40303a1ee09271aab6d2b7b48d1b
Nayan Vishwakarma <52904138+nayan1xyz@users.noreply.github.com>
2021-10-12 13:53:37 +0530

    Added Reverse_words_in_a_given_string.cpp
    Added Reverse_words_in_a_given_string.cpp

--- strings/string_fundamental/C++/Reverse_words_in_a_given_string.cpp
+++ strings/string_fundamental/C++/Reverse_words_in_a_given_string.cpp
@@ -1,4 +1,25 @@
 // C++ code to reverse a string
+/*
+Test Cases:
+1. 
+Input:
+S = i like this program very much
+Output: much very program this like i
+Explanation: After reversing the whole
+string(not individual words), the input
+string becomes
+much very program this like i
+
+2.
+Input:
+S = pqr mno
+Output: mno pqr
+Explanation: After reversing the whole
+string , the input string becomes
+mno pqr
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
