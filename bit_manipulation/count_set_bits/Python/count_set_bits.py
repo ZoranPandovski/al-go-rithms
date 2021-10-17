2427d582b78d4d68d741333f972815d912304657
jaytenenbaum <37470864+jaytenenbaum@users.noreply.github.com>
2021-10-17 22:05:21 +0300

    Update count_set_bits.py

--- bit_manipulation/count_set_bits/Python/count_set_bits.py
+++ bit_manipulation/count_set_bits/Python/count_set_bits.py
@@ -2,14 +2,14 @@
 Input 1:7
 Output 1:3
 
-Explaination:
+Explanation:
 Binary representation of of 7 is "111" and number of set bits(ones) is 3.
 
 
 Input 2:23
 Output 2:4
 
-Explain:
+Explanation:
 Binary representation of of 7 is "10111" and number of set bits(ones) is 4. 
 
 """
