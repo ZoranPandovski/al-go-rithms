5d89d26e1b857a95973583e4c84422e56bdc1ab9
Ajaysinh Rajput <54072689+AjaysinhRajput2802@users.noreply.github.com>
2021-10-21 20:03:43 +0530

    Update Josephus.cpp

--- recursive_algorithms/Josephus_Problem/C++/Josephus.cpp
+++ recursive_algorithms/Josephus_Problem/C++/Josephus.cpp
@@ -3,12 +3,14 @@
 //        Josephus Problem            //
 
 /*
- Problem: There are n people standing in a circle waiting to be executed. The counting out begins at some point in the 
-          circle and proceeds around the circle in a fixed direction. In each step, a certain number of people are skipped and 
-          the next person is executed. The elimination proceeds around the circle (which is becoming smaller and smaller as the 
-          executed people are removed), until only the last person remains, who is given freedom. Given the total number of 
-          persons n and a number k which indicates that k-1 persons are skipped and kth person is killed in circle. The task
-          is to choose the place in the initial circle so that you are the last one remaining and so survive.
+ Problem: There are n people standing in a circle waiting to be executed. The counting out begins at 
+ 	  some point in the circle and proceeds around the circle in a fixed direction. In each step,
+	  a certain number of people are skipped and the next person is executed. The elimination 
+	  proceeds around the circle (which is becoming smaller and smaller as the executed people
+	  are removed), until only the last person remains, who is given freedom. Given the total 
+	  number of persons n and a number k which indicates that k-1 persons are skipped and kth
+	  person is killed in circle. The task is to choose the place in the initial circle so that
+	  you are the last one remaining and so survive.
  
  Solution: Recursive Approach as following:
            josephus(n, k) = (josephus(n - 1, k) + k-1) % n + 1
