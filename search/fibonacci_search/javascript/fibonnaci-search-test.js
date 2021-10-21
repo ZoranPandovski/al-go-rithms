096dcf281675fed9bda75e2549cb06a5b1602f78
Jay <depixellife@gmail.com>
2021-10-21 04:07:01 +0100

    Fix Jasmine method calls

--- search/fibonacci_search/javascript/fibonnaci-search-test.js
+++ search/fibonacci_search/javascript/fibonnaci-search-test.js
@@ -1,14 +1,23 @@
+// use jasmine to run this file
+// https://jasmine.github.io
+//
+// npm install -g jasmine
+// jasmine <this_file>
+
+
+const fibonnaciSearch = require('./fibonnaci-search')
 
 describe('Fibonacci Search', () => {
 
     it ('Search Success', () => {
         let arr = [72, 192, 35, 20, 45, 50, 379, 82,62, 90, 290, 490];
         let n = arr.length;
-        let x = [90, 72, 290, 82];
+        let x = [290, 82, 50];
         x.forEach(test => {
-            let ind = expect(fibonnaciSearch(arr, test, n).toBe(arr.indexOf(test))) ;
+            let ind = fibonnaciSearch(arr, test, n);
+            expect(ind).toBe(arr.indexOf(test))
             if(ind>=0){
-            console.log("Found at index: " + ind);
+            console.log(test + " Found at index: " + ind);
             }else{
             console.log(test + " isn't present in the array");
             }
@@ -17,11 +26,11 @@ describe('Fibonacci Search', () => {
     })
 
     it('Search failed', () => {
-        let arr = [72, 192, 35, 20, 45, 50, 379, 82,62, 90, 290, 490];
+        let arr = [2, 192, 35, 20, 45, 50, 379, 82,62, 90, 290, 490];
         let n = arr.length;
-        let x = [90, 10, 100, 82];
+        let x = [10, 100, 238];
         x.forEach(test => {
-            let ind = expect(fibonnaciSearch(arr, test, n).not.toBe(-1))
+            let ind = expect(fibonnaciSearch(arr, test, n)).toBe(-1)
             expect(ind).not.toBe(Math.sign(ind) == -1)
             if(ind>=0){
             console.log("Found at index: " + ind);
