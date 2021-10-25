ac4f2fc494f60a8067f1ea48f8d5a7f13c17bab2
Harshit Malik <73431489+harshitmalik03@users.noreply.github.com>
2021-10-25 22:00:28 +0530

    Add Word Break Problem in Java.

---
+++ dp/word_break_problem/java/harshitmalik03_Word_Break.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class harshitmalik03_Word_Break{
+
+    public static void main(String[] args)throws Exception{
+        System.out.println("Hello");
+    }
+}
