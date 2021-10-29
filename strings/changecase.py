a43d872697e2164cdf8559901deb066080c35772
SANJANA JAIN <55924455+sanjana1391@users.noreply.github.com>
2021-10-30 01:56:43 +0530

    Create changecase.py

---
+++ strings/changecase.py
@@ -0,0 +1,11 @@
+s = "This is a Hacktoberfest String"
+
+print('String in all lower case-',s.lower())  #lowercase
+
+print('String in all upper case-', s.upper())  #uppercase
+
+print('String is Capitalize-',s.capitalize())  #title_case
+
+print('String is in Title case-',s.title())  #camel_case
+
+print('Swapcase-',s.swapcase())  #Swap the given case
