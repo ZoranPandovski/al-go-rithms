681492c7858062251d7cfbad56693dadccbc7fef
akaknksha <akanksha03@gmail.com>
2022-10-01 14:31:15 +0530

    Correcting for PR comments

--- data_structures/Disjoint_Set_Union/Python/akanksha_disjoint_set.py
+++ data_structures/Disjoint_Set_Union/Python/akanksha_disjoint_set.py
@@ -23,12 +23,11 @@ class UnionFind:
         """
             Return True If u and v are connected (have same root)
         """
-        ans = self.find(u) == self.find(v)
-        print(self._parents)
-        return ans
+        return self.find(u) == self.find(v)
     
     def union(self, u: int, v: int) -> bool:
         """
+            Return True If already connected otherwise False
             Connect node u and v.
             To connect nodes, Assign new parents based on their ranking.
             Hihger ranking root became the parent of lower ranking root 
@@ -43,62 +42,63 @@ class UnionFind:
         else:
             self._parents[root_u] = root_v
             self._ranks[root_v] += 1
-        return True 
+        return False 
 
 
 # Time complexity
 # Find: log(N)
 # Union: log(N)
+def test():
+    arr = [0,1,2,3,4,5]
 
-arr = [0,1,2,3,4,5]
+    union_find = UnionFind(len(arr))
 
-unionFind = UnionFind(len(arr))
-
-unionFind.union(0, 1);
-#  1 2 3 4 5
-#  /
-#  0
-# parents = [1, 1, 2, 3, 4, 5]
-# ranks = [1, 2, 1, 1, 1, 1]
-
-
-unionFind.union(1,2)
-#  1 3 4 5
-#  /\
-#  0 2
-# parents = [1, 1, 1, 3, 4, 5]
-# ranks = [1, 2, 1, 1, 1, 1]
-
-assert(unionFind.connected(0, 2) == True)
-assert(unionFind.connected(1, 2) == True)
-assert(unionFind.connected(5, 4) == False)
+    union_find.union(0, 1);
+    #  1 2 3 4 5
+    #  /
+    #  0
+    # parents = [1, 1, 2, 3, 4, 5]
+    # ranks = [1, 2, 1, 1, 1, 1]
 
 
-unionFind.union(4,5)
-#  1  5  3
-#  /\  \
-#  0 2  4
-# parents = [1, 1, 1, 3, 5, 5]
-# ranks = [1, 2, 1, 1, 1, 2]
+    union_find.union(1,2)
+    #  1 3 4 5
+    #  /\
+    #  0 2
+    # parents = [1, 1, 1, 3, 4, 5]
+    # ranks = [1, 2, 1, 1, 1, 1]
 
-assert(unionFind.connected(5, 4) == True)
-assert(unionFind.connected(0, 4) == False)
+    assert(union_find.connected(0, 2) == True)
+    assert(union_find.connected(1, 2) == True)
+    assert(union_find.connected(5, 4) == False)
 
 
+    union_find.union(4,5)
+    #  1  5  3
+    #  /\  \
+    #  0 2  4
+    # parents = [1, 1, 1, 3, 5, 5]
+    # ranks = [1, 2, 1, 1, 1, 2]
+
+    assert(union_find.connected(5, 4) == True)
+    assert(union_find.connected(0, 4) == False)
 
 
-unionFind.union(1,4)
-#   5  3
-#  / \
-#  1  4
-# /\
-# 0 2 
-# parents = [1, 5, 1, 3, 5, 5]
-# ranks = [1, 2, 1, 1, 1, 3]
+    union_find.union(1,4)
+    #   5  3
+    #  / \
+    #  1  4
+    # /\
+    # 0 2 
+    # parents = [1, 5, 1, 3, 5, 5]
+    # ranks = [1, 2, 1, 1, 1, 3]
 
-assert(unionFind.connected(0, 4) == True)
-# After above check , parents will look like this
-# parents = [5, 5, 1, 3, 5, 5]
+    assert(union_find.connected(0, 4) == True)
+    # After above check , parents will look like this
+    # parents = [5, 5, 1, 3, 5, 5]
 
-assert(unionFind.connected(3, 4) == False)
-assert(unionFind.connected(2, 3) == False)
+    assert(union_find.connected(3, 4) == False)
+    assert(union_find.connected(2, 3) == False)
+
+if __name__ == "__main__":
+    test()
