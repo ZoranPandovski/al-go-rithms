890f933addf08a4ac24469f718b953b487c349a2
RoyCoding8 <royshashwata8@gmail.com>
2022-10-09 21:35:49 +0530

    Create XOR_From_1_to_N.py
    Created the solution program in python language

---
+++ bit_manipulation/xor_of_all_natural_nos_from_1_to_n/Python/XOR_From_1_to_N.py
@@ -0,0 +1,15 @@
+def XOR_from_1_to_N(n: int) -> None:
+    if n%4==0:
+        ans=n
+    elif n%4==1:
+        ans=1
+    elif n%4==2:
+        ans=n+1
+    else:
+        ans=0
+    print(ans)
+
+def main() -> None:
+    XOR_from_1_to_N(int(input("Enter the number : ")))
+
+main()
