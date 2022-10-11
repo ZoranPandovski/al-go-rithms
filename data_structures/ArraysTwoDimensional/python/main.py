f390b347b8e1ca526fcde353f8413c6ef6e6ebd9
VishalVinayRam <vishalvinayram5432@gmail.com>
2022-10-11 16:38:33 +0530

    2d array in python

---
+++ data_structures/ArraysTwoDimensional/python/main.py
@@ -0,0 +1,17 @@
+import numpy
+
+
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+for x in arr:
+    print(x)
+
+# arr = [[]]
+# for i in range(m):
+#     k = []
+#     for j  in range(n):
+#         q= int(input("Enter the element at %d",j))
+#         k.append(q)
+#     arr.append(k)
+
+# for x in range(m):
+#     print(ar)
