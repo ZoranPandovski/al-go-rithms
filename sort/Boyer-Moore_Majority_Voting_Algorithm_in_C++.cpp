30afdbcfa9ae96d57c2710880b6d0aac13aaab4b
Shivesh Kumar <87690329+shivesh41kr@users.noreply.github.com>
2022-10-11 18:56:57 +0530

    Boyer-Moore Majority Voting Algorithm

---
+++ sort/Boyer-Moore_Majority_Voting_Algorithm_in_C++.cpp
@@ -0,0 +1,48 @@
+//Boyer-Moore Majority Voting Algorithm
+
+#include <iostream>
+using namespace std;
+// Function to find majority element
+int findMajority(int arr[], int n)
+{
+    int i, candidate = -1, votes = 0;
+    // Finding majority candidate
+    for (i = 0; i < n; i++) {
+        if (votes == 0) {
+            candidate = arr[i];
+            votes = 1;
+        }
+        else {
+            if (arr[i] == candidate)
+                votes++;
+            else
+                votes--;
+        }
+    }
+    int count = 0;
+    // Checking if majority candidate occurs more than n/2
+    // times
+    for (i = 0; i < n; i++) {
+        if (arr[i] == candidate)
+            count++;
+    }
+ 
+    if (count > n / 2)
+        return candidate;
+    return -1;
+}
+int main()
+{
+    int arr[] = { 1, 1, 1, 1, 2, 3, 4 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int majority = findMajority(arr, n);
+    cout << " The majority element is : " << majority;
+    return 0;
+}
+
+/*
+
+Time Complexity: O(n) ( For two passes over the array )
+Space Complexity: O(1)
+
+*/
