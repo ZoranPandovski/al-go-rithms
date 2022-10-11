1fc664e7557258e825e8e9bb66e139bc82a091cd
VishalVinayRam <vishalvinayram5432@gmail.com>
2022-10-11 16:56:34 +0530

    multi dimensional array

--- data_structures/ArraysTwoDimensional/python/main.py
+++ data_structures/ArraysTwoDimensional/python/main.py
@@ -1,17 +1,7 @@
-import numpy
 
 
-arr = [[1,2,3],[4,5,6],[7,8,9]]
-for x in arr:
-    print(x)
-
-# arr = [[]]
-# for i in range(m):
-#     k = []
-#     for j  in range(n):
-#         q= int(input("Enter the element at %d",j))
-#         k.append(q)
-#     arr.append(k)
-
-# for x in range(m):
-#     print(ar)
+size = int(input("Enter the size of the elements")) #the size for the matrix
+array_input = []
+for x in range(size):
+    array_input.append([int(y) for y in input().split()])
+print(array_input)
