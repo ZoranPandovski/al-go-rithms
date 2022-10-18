74d6859b5fc07d6c71b36394d73b36f27314116d
Shreyansh <95679948+IceCurrent@users.noreply.github.com>
2022-10-18 10:52:36 +0530

    Added detailed implementation of Ceasar Cipher
    Added detailed implementation of basic Ceasar Cipher using Python 2.7

---
+++ cryptography/caesar_cipher/python/IceCurrent_Caesar _Cipher/IceCurrent_Ceasar_Cipher.py
@@ -0,0 +1,264 @@
+
+## Caesar Cipher
+
+import string
+
+WORDLIST_FILENAME = "words.txt"
+
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = line.split()
+    print "  ", len(wordlist), "words loaded."
+    return wordlist
+
+
+wordlist = load_words()
+
+# Encryption
+
+def build_coder(shift):
+    """
+    Returns a dict that can apply a Caesar cipher to a letter.
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation and numbers.
+
+    shift: -27 < int < 27
+    returns: dict
+
+    Example:
+    >>> build_coder(3)
+    {' ': 'c', 'A': 'D', 'C': 'F', 'B': 'E', 'E': 'H', 'D': 'G', 'G': 'J',
+    'F': 'I', 'I': 'L', 'H': 'K', 'K': 'N', 'J': 'M', 'M': 'P', 'L': 'O',
+    'O': 'R', 'N': 'Q', 'Q': 'T', 'P': 'S', 'S': 'V', 'R': 'U', 'U': 'X',
+    'T': 'W', 'W': 'Z', 'V': 'Y', 'Y': 'A', 'X': ' ', 'Z': 'B', 'a': 'd',
+    'c': 'f', 'b': 'e', 'e': 'h', 'd': 'g', 'g': 'j', 'f': 'i', 'i': 'l',
+    'h': 'k', 'k': 'n', 'j': 'm', 'm': 'p', 'l': 'o', 'o': 'r', 'n': 'q',
+    'q': 't', 'p': 's', 's': 'v', 'r': 'u', 'u': 'x', 't': 'w', 'w': 'z',
+    'v': 'y', 'y': 'a', 'x': ' ', 'z': 'b'}
+    """
+    assert type(shift) == int
+    assert -27 < shift < 27
+    s = abs(shift)
+
+    ref_dict_cap = {}
+    int_key = 1
+    alphabet_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+    for x in alphabet_str:
+        ref_dict_cap[int_key] = x
+        int_key += 1
+
+    cipher_dict_cap = {}
+    int_key2 = 1
+    for x in alphabet_str:
+        if int_key2 <= 28:
+            cipher_dict_cap[x] = ref_dict_cap[int_key2 + s]
+            int_key2 += 1
+
+    ref_dict_low = {}
+    int_key3 = 1
+    for x in alphabet_str.lower():
+        ref_dict_low[int_key3] = x
+        int_key3 += 1
+
+    cipher_dict_low = {}
+    int_key4 = 1
+    for x in alphabet_str.lower():
+        if int_key4 <= 28:
+            cipher_dict_low[x] = ref_dict_low[int_key4 + s]
+            int_key4 += 1
+
+    cipher_dict_cap.update(cipher_dict_low)
+
+    if shift >= 0:
+        return cipher_dict_cap
+
+    else:
+        cipher_dict2 = {}
+        int_key5 = 0
+        for x in cipher_dict_cap.keys():
+            cipher_dict2[cipher_dict_cap.values()[int_key5]] = x
+            int_key5 += 1
+
+        return cipher_dict2
+
+
+def build_encoder(shift):
+    """
+    Returns a dict that can be used to encode a plain text. For example, you
+    could encrypt the plain text by calling the following commands
+    >>>encoder = build_encoder(shift)
+    >>>encrypted_text = apply_coder(plain_text, encoder)
+
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation and numbers.
+
+    shift: 0 <= int < 27
+    returns: dict
+
+    Example:
+    >>> build_encoder(3)
+    {' ': 'c', 'A': 'D', 'C': 'F', 'B': 'E', 'E': 'H', 'D': 'G', 'G': 'J',
+    'F': 'I', 'I': 'L', 'H': 'K', 'K': 'N', 'J': 'M', 'M': 'P', 'L': 'O',
+    'O': 'R', 'N': 'Q', 'Q': 'T', 'P': 'S', 'S': 'V', 'R': 'U', 'U': 'X',
+    'T': 'W', 'W': 'Z', 'V': 'Y', 'Y': 'A', 'X': ' ', 'Z': 'B', 'a': 'd',
+    'c': 'f', 'b': 'e', 'e': 'h', 'd': 'g', 'g': 'j', 'f': 'i', 'i': 'l',
+    'h': 'k', 'k': 'n', 'j': 'm', 'm': 'p', 'l': 'o', 'o': 'r', 'n': 'q',
+    'q': 't', 'p': 's', 's': 'v', 'r': 'u', 'u': 'x', 't': 'w', 'w': 'z',
+    'v': 'y', 'y': 'a', 'x': ' ', 'z': 'b'}
+    """
+
+    assert type(shift) == int
+    assert 0 <= shift < 27
+
+    return build_coder(shift)
+
+
+def build_decoder(shift):
+    """
+    Returns a dict that can be used to decode an encrypted text. For example, you
+    could decrypt an encrypted text by calling the following commands
+    >>>encoder = build_encoder(shift)
+    >>>encrypted_text = apply_coder(plain_text, encoder)
+    >>>decrypted_text = apply_coder(plain_text, decoder)
+
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation and numbers.
+
+    shift: 0 <= int < 27
+    returns: dict
+
+    Example:
+    >>> build_decoder(3)
+    {' ': 'x', 'A': 'Y', 'C': ' ', 'B': 'Z', 'E': 'B', 'D': 'A', 'G': 'D',
+    'F': 'C', 'I': 'F', 'H': 'E', 'K': 'H', 'J': 'G', 'M': 'J', 'L': 'I',
+    'O': 'L', 'N': 'K', 'Q': 'N', 'P': 'M', 'S': 'P', 'R': 'O', 'U': 'R',
+    'T': 'Q', 'W': 'T', 'V': 'S', 'Y': 'V', 'X': 'U', 'Z': 'W', 'a': 'y',
+    'c': ' ', 'b': 'z', 'e': 'b', 'd': 'a', 'g': 'd', 'f': 'c', 'i': 'f',
+    'h': 'e', 'k': 'h', 'j': 'g', 'm': 'j', 'l': 'i', 'o': 'l', 'n': 'k',
+    'q': 'n', 'p': 'm', 's': 'p', 'r': 'o', 'u': 'r', 't': 'q', 'w': 't',
+    'v': 's', 'y': 'v', 'x': 'u', 'z': 'w'}
+    """
+    assert type(shift) == int
+    assert 0 <= shift < 27
+
+    return build_coder(-shift)
+
+
+def apply_coder(text, coder):
+    """
+    Applies the coder to the text. Returns the encoded text.
+
+    text: string
+    coder: dict with mappings of characters to shifted characters
+    returns: text after mapping coder chars to original text
+
+    Example:
+    >>> apply_coder("Hello, world!", build_encoder(3))
+    'Khoor,czruog!'
+    >>> apply_coder("Khoor,czruog!", build_decoder(3))
+    'Hello, world!'
+    """
+    assert type(text) == str
+    assert type(coder) == dict
+
+    coded_text = ''
+    for x in text:
+        if x in coder.keys():
+            coded_text += coder[x]
+        else:
+            coded_text += x
+
+    return coded_text
+
+
+def apply_shift(text, shift):
+    """
+    Given a text, returns a new text Caesar shifted by the given shift
+    offset. The empty space counts as the 27th letter of the alphabet,
+    so spaces should be replaced by a lowercase letter as appropriate.
+    Otherwise, lower case letters should remain lower case, upper case
+    letters should remain upper case, and all other punctuation should
+    stay as it is.
+
+    text: string to apply the shift to
+    shift: amount to shift the text
+    returns: text after being shifted by specified amount.
+
+    Example:
+    >>> apply_shift('This is a test.', 8)
+    'Apq hq hiham a.'
+    """
+    assert type(text) == str
+    assert type(shift) == int
+    assert -27 < shift < 27
+
+    coder = build_coder(shift)
+    return apply_coder(text, coder)
+
+
+# Codebreaking
+
+def find_best_shift(wordlist, text):
+    """
+    Finds the best shift(if that exixts) by which we could decode a
+    a given text to plaintext.
+
+    text: string
+    returns: 0 <= int 27
+
+    Example:
+    >>> s = apply_coder('Hello, world!', build_encoder(8))
+    >>> s
+    'Pmttw,hdwztl!'
+    >>> find_best_shift(wordlist, s) returns
+    8
+    >>> apply_coder(s, build_decoder(8)) returns
+    'Hello, world!'
+    """
+    assert type(text) == str
+
+    clean_text = ''
+    for x in text:
+        if x in build_coder(2).keys():
+            clean_text += x
+
+    clean_text = clean_text.lower()
+
+    word_set = []
+
+    for shift in range(27):
+        decrypted_version = apply_shift(clean_text, -shift)
+
+        for x in decrypted_version:
+            if x == ' ':
+                index = decrypted_version.find(x)
+
+                word_set.append(decrypted_version[:index])
+                decrypted_version = decrypted_version[index + 1:]
+
+        word_set.append(decrypted_version)
+
+        good_index = 0
+        for i in word_set:
+            if i in wordlist:
+                good_index += 1
+
+        if good_index == len(word_set):
+            return shift
+
+        else:
+            del word_set[:]
+
+    return "can't find any good shift"
