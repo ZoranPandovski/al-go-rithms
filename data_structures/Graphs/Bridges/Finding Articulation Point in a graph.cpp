0540c9371b9a354bb31ab717fdae3004f3f09b5d
the-good-boy <shivam.awasthi702@gmail.com>
2022-10-25 18:21:39 +0530

    articulation points

---
+++ data_structures/Graphs/Bridges/Finding Articulation Point in a graph.cpp
@@ -0,0 +1,42 @@
+int n; // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children=0;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] >= tin[v] && p!=-1)
+                IS_CUTPOINT(v);
+            ++children;
+        }
+    }
+    if(p == -1 && children > 1)
+        IS_CUTPOINT(v);
+}
+
+void find_cutpoints() {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs (i);
+    }
+}
+
+// Main function is find_cutpoints; it performs necessary initialization and starts depth first search in each connected component of the graph.
+
+// Function IS_CUTPOINT(a) is some function that will process the fact that vertex
+// is an articulation point, for example, print it (Caution that this can be called multiple times for a vertex).
