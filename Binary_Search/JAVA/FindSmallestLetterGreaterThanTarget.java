136cf17c793db5c4c3cea4ba079722c9e4355ed9
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2022-10-28 15:20:30 +0530

    Update FindSmallestLetterGreaterThanTarget.java

--- Binary_Search/JAVA/FindSmallestLetterGreaterThanTarget.java
+++ Binary_Search/JAVA/FindSmallestLetterGreaterThanTarget.java
@@ -16,3 +16,19 @@ class FindSmallestLetterGreaterThanTarget {
         return letters[start % letters.length];      
     }
 }
+
+//TEST CASES
+/*Example 1:
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+Example 2:
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+Example 3:
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].*/
