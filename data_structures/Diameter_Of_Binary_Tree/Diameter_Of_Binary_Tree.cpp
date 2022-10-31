379dde473adad31c3cf312c96c6a06ee18f0ba54
naitik360 <74945351+naitik360@users.noreply.github.com>
2022-10-31 22:47:54 +0530

    Update Diameter_Of_Binary_Tree.cpp

--- data_structures/Diameter_Of_Binary_Tree/Diameter_Of_Binary_Tree.cpp
+++ data_structures/Diameter_Of_Binary_Tree/Diameter_Of_Binary_Tree.cpp
@@ -6,24 +6,26 @@ struct node
 	struct node *left, *right;
 };
 struct node *newNode(int data);
-int max(int a, int b) { return (a > b) ? a : b; }
-int height(struct node *node);
-int diameter(struct node *tree)
-{
-	if (tree == NULL)
-		return 0;
-	int lheight = height(tree->left);
-	int rheight = height(tree->right);
-	int ldiameter = diameter(tree->left);
-	int rdiameter = diameter(tree->right);
-	return max(lheight + rheight + 1,
-			   max(ldiameter, rdiameter));
+
+int ans=0;
+int find(struct node* root)
+{  
+    if(!root) return 0;
+
+     int l = find(root->left);
+     int r = find(root->right);
+
+     ans=max(ans,l+r+1);
+
+     return max(l,r)+1;
 }
-int height(struct node *node)
-{
-	if (node == NULL)
-		return 0;
-	return 1 + max(height(node->left), height(node->right));
+int diameter(struct node *tree)
+{       
+	ans=0;
+	if(!root) 
+          return 0;
+        find(root);
+          return ans;
 }
 struct node *newNode(int data)
 {
